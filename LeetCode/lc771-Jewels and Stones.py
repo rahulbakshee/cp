925b640f97878e4a24d604b06afe7a270870569b
Rahul Bakshee <rhlbks@gmail.com>
2021-01-03 16:54:28 +0530

    Update lc771-Jewels and Stones.py

--- LeetCode/lc771-Jewels and Stones.py
+++ LeetCode/lc771-Jewels and Stones.py
@@ -1,4 +1,4 @@
-# https://leetcode.com/problems/jewels-and-stones/submissions/
+# https://leetcode.com/problems/jewels-and-stones/
 
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
