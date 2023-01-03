64eee9d05eb9aab65c7070597951d6d70d266949
Rahul Bakshee <rhlbks@gmail.com>
2023-01-03 00:11:49 -0800

    Update lc001-Two Sum.py

--- LeetCode/lc001-Two Sum.py
+++ LeetCode/lc001-Two Sum.py
@@ -1,12 +1,24 @@
 # https://leetcode.com/problems/two-sum/
 
+# bruteforce
+# space O(1) time O(n^2)
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dic = {} # val : index
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                   return [i, j]
+        
+# Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+# hashmap/dict
+# space O(n) time O(n)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
 
-        for i, n in enumerate(nums):
-            diff = target - n
-            if diff in dic:
-                return [dic[diff], i]
-            dic[n] = i
-        return
+        for index, num in enumerate(nums):
+            dif = target - num
+
+            if dif in hashmap:
+                return [hashmap[dif], index]
+            hashmap[num] = index
