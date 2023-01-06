21b2e8760a5653636aad3c81abdf07eaf0188970
Rahul Bakshee <rhlbks@gmail.com>
2023-01-05 17:19:30 -0800

    Create lc0167-Two Sum II - Input Array Is Sorted.py

---
+++ LeetCode/lc0167-Two Sum II - Input Array Is Sorted.py
@@ -0,0 +1,46 @@
+# https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+# # bruteforce -not utilizing sortedness
+# # space O(1), time O(n^2)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i+1, j+1]
+
+
+# still not utilizing sortedness
+# space O(n), time O(n)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+
+        for index, num in enumerate(nums):
+            diff = target - num
+
+            if diff in hashmap:
+               return [hashmap[diff]+1, index+1]
+            hashmap[num] = index
+
+
+# Your solution must use only constant extra space.
+# time to uitlize the sortedness of given array
+# two pointer solution
+# space O(1), time O(n)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        left, right = 0, len(nums)-1
+
+        while left < right:
+            s = nums[left] + nums[right]
+
+            if s == target:
+                return [left+1, right+1]
+
+            if s < target:
+                left += 1
+            else:
+                right -= 1
+                
+                
