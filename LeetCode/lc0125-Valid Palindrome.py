e67c249ff771f2f5a9b670ec16de7c7ea87d0a4f
Rahul Bakshee <rhlbks@gmail.com>
2024-05-31 18:31:34 -0400

    Update lc0125-Valid Palindrome.py

--- LeetCode/lc0125-Valid Palindrome.py
+++ LeetCode/lc0125-Valid Palindrome.py
@@ -19,7 +19,7 @@ class Solution:
 
         return news == news[::-1]
 
-
+# using python inbuilt  isalnum function
 # time O(n), space: O(1)
 class Solution:
     def isPalindrome(self, s: str) -> bool:
@@ -38,3 +38,27 @@ class Solution:
                 return False
         return True
 
+# time-O(n), space-O(1)
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        def isInRange(char):
+            if ((ord("0") <= ord(char) <= ord("9")) or
+                (ord("A") <= ord(char) <= ord("Z")) or
+                (ord("a") <= ord(char) <= ord("z"))):
+                return True
+            else:
+                return False
+        left, right = 0, len(s)-1
+
+        while left < right:
+            while left < right and not isInRange(s[left]):
+                left += 1
+            while left < right and not isInRange(s[right]):
+                right -= 1
+            if s[left].lower() != s[right].lower():
+                return False
+            else:
+                left  += 1
+                right -= 1
+        return True
