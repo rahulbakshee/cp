14dfb3f98e1144f0c17fdfab9476874b82d3cf05
Rahul Bakshee <rhlbks@gmail.com>
2024-06-30 00:10:13 -0700

    Create lc0704-Binary Search.py

---
+++ LeetCode/lc0704-Binary Search.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/binary-search/description/
+
+# time:O(log n), space: O(1)
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+
+        while left <= right:
+            mid  = left + (right -left)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid+1
+            else:
+                right = mid-1
+
+        return -1
