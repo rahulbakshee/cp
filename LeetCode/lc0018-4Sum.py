ffb94d127e18b1c9cf416900169d63668690a8ff
Rahul Bakshee <rhlbks@gmail.com>
2024-07-16 09:17:59 -0700

    Create lc0018-4Sum.py

---
+++ LeetCode/lc0018-4Sum.py
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/4sum/description/
+
+# taken from neetcode - need to revisit 
+# time:O(n**3), space:O(n)
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        res, quad = [], []
+
+        def kSum(k:int, start:int, target:int):
+            if k!=2:
+                for i in range(start, len(nums)-k+1):
+                    if i>start and nums[i] == nums[i-1]:
+                        continue
+                    quad.append(nums[i])
+                    kSum(k-1, i+1, target-nums[i])
+                    quad.pop()
+
+                return
+            left, right = start, len(nums)-1
+            while left < right:
+                if nums[left] + nums[right] < target:
+                    left += 1
+                elif nums[left] + nums[right] > target:
+                    right -= 1
+                else:
+                    res.append(quad + [nums[left],  nums[right]])
+                    left += 1
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1
+        kSum(4, 0, target)
+        
+        return res
