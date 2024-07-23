6da37c08a20c304c374a3a7e5c62d25ab2258803
Rahul Bakshee <rhlbks@gmail.com>
2024-07-23 13:25:34 -0700

    Create lc0141-Linked List Cycle.py

---
+++ LeetCode/lc0141-Linked List Cycle.py
@@ -0,0 +1,44 @@
+# using hash set
+# time:O(n), space:O(n)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        visited = set()
+        curr = head
+
+        while curr:
+            if curr in visited:
+                return True
+        
+            visited.add(curr)
+            curr = curr.next
+        return False
+
+
+
+
+# two pointers
+# time:O(n), space:O(1)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
+        return False
