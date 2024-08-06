156c03d1769d5ad1549223e8a5b8d76170acc0cb
Rahul Bakshee <rhlbks@gmail.com>
2024-08-06 15:39:56 -0700

    Update lc0703-Kth Largest Element in a Stream.py

--- LeetCode/lc0703-Kth Largest Element in a Stream.py
+++ LeetCode/lc0703-Kth Largest Element in a Stream.py
@@ -1,5 +1,15 @@
-# adding/inserting element to heap : time -O(log n )
-# accessing the min/max element: time -O(1)
+# Given N as the length of nums and M as the number of calls to add(),
+
+# Time complexity: O(N⋅log(N)+M⋅log(k))
+
+# The time complexity is split into two parts. First, the constructor needs to turn nums into a heap of size k. In Python, heapq.heapify() can turn nums into a heap in O(N) time. Then, we need to remove from the heap until there are only k elements in it, which means removing N - k elements. Since k can be, say 1, in terms of big O this is N operations, with each operation costing log(N). Therefore, the constructor costs O(N+N⋅log(N))=O(N⋅log(N)).
+
+# Next, every call to add() involves adding an element to heap and potentially removing an element from heap. Since our heap is of size k, every call to add() at worst costs O(2∗log(k))=O(log(k)). That means M calls to add() costs O(M⋅log(k)).
+
+# Space complexity: O(N)
+
+# The only extra space we use is the heap. While during add() calls we limit the size of the heap to k, in the constructor we start by converting nums into a heap, which means the heap will initially be of size N.
+
 
 class KthLargest:
 
