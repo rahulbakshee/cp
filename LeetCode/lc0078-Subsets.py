5a7d3ecd0bdeb27e686fa940cbf8c638bcd9052e
Rahul Bakshee <rhlbks@gmail.com>
2024-08-16 14:32:46 -0700

    Update lc0078-Subsets.py

--- LeetCode/lc0078-Subsets.py
+++ LeetCode/lc0078-Subsets.py
@@ -4,16 +4,23 @@
 # For Space it is O(n) if you don't count the output array, because the size of the function call stack will be O(n).
 # Meaning we have to call the recursive function n times in a row, before it returns.
 
+
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        def backtrack(start, end, temp):
-            result.append(temp[:])
-            
-            for i in range(start, end):
-                temp.append(nums[i])
-                backtrack(i+1, len(nums), temp)
-                temp.pop()
+        result, sol = [], []
 
-        result = []    
-        backtrack(start=0, end=len(nums), temp=[])
+        def backtrack(i):
+            if i >= len(nums):
+                result.append(sol[:])
+                return
+
+            # take it
+            sol.append(nums[i])
+            backtrack(i+1)
+            
+            # don't take it
+            sol.pop()
+            backtrack(i+1)
+
+        backtrack(0)
         return result
