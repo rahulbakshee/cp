c9d70af4d2e7cde53c2b1b04ff1f4552134d9866
Rahul Bakshee <rhlbks@gmail.com>
2024-08-19 14:46:20 -0700

    Create lc0119-Pascal's Triangle II.py

---
+++ LeetCode/lc0119-Pascal's Triangle II.py
@@ -0,0 +1,31 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0:
+            return [1]
+        
+        result = []
+        for row in range(rowIndex+1):
+            temp = []
+            for col in range(row+1):
+                if col == 0 or col == row:
+                    temp.append(1)
+                
+                else:
+                    val = result[-1][col-1] + result[-1][col] 
+                    temp.append(val)          
+                
+            result.append(temp)         
+            
+        return result[-1]
+            
+            
+            
+"""  row     temp     
+  ->  0      1
+      1      1 1
+      2      1 2 1
+      3      1 3 3 1
+      4      1 4 6 4 1
+            
+            result
+"""
