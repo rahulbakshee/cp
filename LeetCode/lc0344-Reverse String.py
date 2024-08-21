8889cecdc7791b7dc47f2bccc41bd56a4475f71c
Rahul Bakshee <rhlbks@gmail.com>
2024-08-21 14:39:11 -0700

    Update lc0344-Reverse String.py

--- LeetCode/lc0344-Reverse String.py
+++ LeetCode/lc0344-Reverse String.py
@@ -1,31 +1,48 @@
+# kind of cheating - time:O(n), space:O(n)
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        s.reverse()
+
+# kind of cheating - time:O(n), space:O(n)
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        s[:] = s[::-1]
+
+
+
+
 # using stack - time:O(n), space:O(n)
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
         stack = []
+        
+        # push to stack
         for char in s:
             stack.append(char)
-        i = 0
-        while stack:
+        # pop from stack and place into the index
+        for i in range(len(s)):
             s[i] = stack.pop()
-            i += 1
-            
-        
 
-
+  
 
 # two pointers - time:O(n), space:O(1)
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        
         left, right = 0, len(s)-1
         while left < right:
             s[left], s[right] = s[right], s[left]
             left += 1
             right -= 1
 
+
+
+      
+# recursive  - time:O(n), space:O(n)-recursive call stack
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        def recurse(l, r):
+            if l < r:
+                s[l], s[r] = s[r], s[l]
+                recurse(l+1, r-1)
+
+        recurse(0, len(s)-1)
