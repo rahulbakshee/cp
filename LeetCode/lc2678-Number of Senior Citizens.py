2fb1a2b3721252fc745fa280468370e2bdb70158
Rahul Bakshee <rhlbks@gmail.com>
2024-08-26 13:02:17 -0700

    Create lc2678-Number of Senior Citizens.py

---
+++ LeetCode/lc2678-Number of Senior Citizens.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        counter = 0
+        for detail in details:
+            if int(detail[11:13]) > 60:
+                counter += 1
+
+        return counter
