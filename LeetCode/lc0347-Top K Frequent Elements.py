65668ac64158c18d54beab3489c06bd3acdd8086
Rahul Bakshee <rhlbks@gmail.com>
2024-08-30 13:58:57 -0700

    Update lc0347-Top K Frequent Elements.py

--- LeetCode/lc0347-Top K Frequent Elements.py
+++ LeetCode/lc0347-Top K Frequent Elements.py
@@ -1,4 +1,35 @@
-# https://leetcode.com/problems/top-k-frequent-elements/description/
+# good solutions at  - https://leetcode.com/problems/top-k-frequent-elements/solutions/1502514/c-python-2-solutions-maxheap-bucket-sort-clean-concise/
+
+
+# using heap - 
+# time:O(n) freq + O(n) heapify best OR O(nlogn) heapify worst case + O(klogn) heappop
+# space:O(n) freq, + O(n) heap
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = {}
+        for num in nums:
+            if num in freq:
+                freq[num] += 1
+            else:
+                freq[num] = 1
+
+        # freq: key:num, val:occurances {1:3, 2:5, 7:1}
+
+        # maxHeap for storing the (occurances, num)
+
+        maxHeap = [(-1*occur, key) for key, occur in freq.items()]
+        heapify(maxHeap)
+
+
+        result = []
+        for _ in range(k):
+            occur, key = heappop(maxHeap)
+            result.append(key)
+
+        return result
+
+
+
 
 # bucket sort - 
 # time-O(n), space-O(n)
