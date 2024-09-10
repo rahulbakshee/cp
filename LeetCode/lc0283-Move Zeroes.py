215cfa7dd848c263b16ca319b0be35ccc542741d
Rahul Bakshee <rhlbks@gmail.com>
2024-09-09 23:08:23 -0700

    Update lc0283-Move Zeroes.py

--- LeetCode/lc0283-Move Zeroes.py
+++ LeetCode/lc0283-Move Zeroes.py
@@ -46,3 +46,17 @@ L
 R
 
 """
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[index], nums[i] = nums[i], nums[index]
+                index += 1
+
