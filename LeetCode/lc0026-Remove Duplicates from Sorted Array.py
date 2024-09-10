8098fb2b8abe0aed6bf24379f1771d34674e2945
Rahul Bakshee <rhlbks@gmail.com>
2024-09-09 21:42:39 -0700

    Update lc0026-Remove Duplicates from Sorted Array.py

--- LeetCode/lc0026-Remove Duplicates from Sorted Array.py
+++ LeetCode/lc0026-Remove Duplicates from Sorted Array.py
@@ -24,3 +24,15 @@ class Solution:
                 right += 1
 
         return left+1
+
+
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        index = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[index] = nums[i]
+                index += 1
+        return index
