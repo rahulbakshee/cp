a6b13f943ac67419241152450ba45c88b4746b6a
Rahul Bakshee <rhlbks@gmail.com>
2024-09-10 21:05:29 -0700

    Update lc0203-Remove Linked List Elements.py

--- LeetCode/lc0203-Remove Linked List Elements.py
+++ LeetCode/lc0203-Remove Linked List Elements.py
@@ -23,18 +23,14 @@ class Solution:
 
 
 
-# time:O(n), space:O(n)
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+# recursive DFS - time:O(n), space:O(n)
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
         # base case
         if not head:
-            return
+            return 
         
+        # recursion
         head.next = self.removeElements(head.next, val)
         if head.val == val:
             return head.next
