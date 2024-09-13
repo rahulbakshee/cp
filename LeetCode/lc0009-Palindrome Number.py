6942cad7d9065934bc6c1f27802aa54d147d2f2b
Rahul Bakshee <rhlbks@gmail.com>
2024-09-12 18:47:48 -0700

    Update lc0009-Palindrome Number.py

--- LeetCode/lc0009-Palindrome Number.py
+++ LeetCode/lc0009-Palindrome Number.py
@@ -1,5 +1,27 @@
-# https://leetcode.com/problems/palindrome-number/
+# time:O(n), space:O(reversing)
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return str(x) == str(x)[::-1]
 
+
+
+# optimal
+# time:O(n), space:O(1)
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0:
+            return False
+        
+        original = x
+        backwards = 0
+        while x:
+            backwards = backwards*10 + x%10
+            x = x//10
+
+        return original == backwards
+
+
+# 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         l = []
