a7c4182a7a8acee72337fa4b387858a5bcf439f5
Rahul Bakshee <rhlbks@gmail.com>
2024-09-15 10:57:27 -0700

    Update lc0036-Valid Sudoku.py

--- LeetCode/lc0036-Valid Sudoku.py
+++ LeetCode/lc0036-Valid Sudoku.py
@@ -5,17 +5,18 @@ class Solution:
         cols = collections.defaultdict(set)
         squares = collections.defaultdict(set)
 
+        # iterate over each cell
         for r in range(9):
             for c in range(9):
+                # if it is not a number then continue
                 if board[r][c] == ".":
                     continue
-                if board[r][c] in rows[r]:
+
+                # if it is a number, check if we have seen it in above sets
+                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r//3, c//3)]:
                     return False
-                if board[r][c] in cols[c]:
-                    return False
-                if board[r][c] in squares[(r//3, c//3)]:
-                    return False
-                
+
+                # if not seen anywhere, then add it to the above sets
                 rows[r].add(board[r][c])
                 cols[c].add(board[r][c])
                 squares[(r//3, c//3)].add(board[r][c])
