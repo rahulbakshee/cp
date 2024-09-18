c71185210663227147648a28c28fe854c8ee1b55
Rahul Bakshee <rhlbks@gmail.com>
2024-09-18 10:53:47 -0700

    Update lc0733-Flood Fill.py

--- LeetCode/lc0733-Flood Fill.py
+++ LeetCode/lc0733-Flood Fill.py
@@ -47,3 +47,25 @@ class Solution:
 
 
         return image
+
+
+
+# DFS using stack - time:O(m*n), space:O(m*n)+callstack
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        rows = len(image)
+        cols = len(image[0])
+        curr_color = image[sr][sc]
+        new_color = color
+
+        if curr_color != new_color:
+            stack = [(sr, sc)]
+            while stack:
+                r,c = stack.pop()
+                image[r][c] = new_color
+
+                for x,y in [(r+1,c),(r-1,c),(r,c+1),(r,c-1)]:
+                    if 0<=x<rows and 0<=y<cols and image[x][y] == curr_color and image[x][y] != new_color:
+                        stack.append((x,y))
+
+        return image
