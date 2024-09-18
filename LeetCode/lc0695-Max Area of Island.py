0fb4593472732f063a5bdebc24ea04f1e8635a7f
Rahul Bakshee <rhlbks@gmail.com>
2024-09-18 13:59:31 -0700

    Create lc0695-Max Area of Island.py

---
+++ LeetCode/lc0695-Max Area of Island.py
@@ -0,0 +1,59 @@
+# DFS - time:O(m*n), space:O(m*n) + recusrive callstack
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        visited = set()
+        max_area = 0
+
+
+        def dfs(r,c):
+            if r<0 or c<0 or r>= rows or c>=cols or (r,c) in visited or grid[r][c] == 0:
+                return 0
+
+            visited.add((r,c))
+            return 1+ dfs(r+1,c) + dfs(r,c+1) + dfs(r-1,c) + dfs(r,c-1)
+
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1 and (i,j) not in visited:
+                    curr_area = dfs(i, j)
+                    max_area = max(max_area, curr_area)
+
+        return max_area
+
+
+
+# bfs - queue - time:O(m*n), space:O(m*n)
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        visited = set()
+        max_area = 0
+
+        def bfs(r,c):
+            q = collections.deque()
+            q.append((r,c))
+            visited.add((r,c))
+            area = 0
+
+            while q:
+                row,col = q.popleft()
+                area += 1
+                for x,y in [(row+1,col), (row-1,col), (row,col+1), (row,col-1)]:
+                    if 0<=x<rows and 0<=y<cols and (x,y) not in visited and grid[x][y]==1:
+                        q.append((x,y))
+                        visited.add((x,y))
+            return area
+
+        
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1 and (i,j) not in visited:
+                    curr_area = bfs(i,j)
+                    max_area = max(max_area, curr_area)
+
+        return max_area
+
