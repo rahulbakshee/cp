2263b95ad3c9d8a15013baab1d86ec1fe0b3a70d
Rahul Bakshee <rhlbks@gmail.com>
2024-09-19 21:37:05 -0700

    Create lc0133-Clone Graph.py

---
+++ LeetCode/lc0133-Clone Graph.py
@@ -0,0 +1,30 @@
+# DFS - time:O(n) n=Edges+vertices), space:O(v)
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        oldToNew = {}
+
+        def dfs(node):
+            if not node:
+                return 
+            if node in oldToNew:
+                return oldToNew[node]
+
+            copy = Node(node.val)
+            oldToNew[node] = copy
+            for nei in node.neighbors:
+                copy.neighbors.append(dfs(nei))
+
+            return copy
+
+        return dfs(node)
+
+LOOK FOR OTEHR SOLUTIONS ON THE sOLUTIONS PAGE
