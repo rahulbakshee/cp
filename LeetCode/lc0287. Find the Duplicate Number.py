e0cef21e8c8c92617c86067e1e324f269e4653f7
Rahul Bakshee <rhlbks@gmail.com>
2024-09-28 15:22:21 -0700

    Update lc0287. Find the Duplicate Number.py

--- LeetCode/lc0287. Find the Duplicate Number.py
+++ LeetCode/lc0287. Find the Duplicate Number.py
@@ -1,5 +1,4 @@
-#floyd's algo
-
+#  MEMORIZE THIS - FLOYD'S ALGO - HARE AND TORTOISE ALGO
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         slow, fast = 0, 0
@@ -16,3 +15,6 @@ class Solution:
             slow2 = nums[slow2]
             if slow == slow2:
                 return slow
+
+        
+    
