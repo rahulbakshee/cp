f525af3b9d45d07978886a82a72d58bbb5b5289a
Rahul Bakshee <rhlbks@gmail.com>
2024-10-03 15:34:18 -0700

    Update lc0853-Car Fleet.py

--- LeetCode/lc0853-Car Fleet.py
+++ LeetCode/lc0853-Car Fleet.py
@@ -11,3 +11,23 @@ class Solution:
                 stack.pop()
             
         return len(stack)
+
+
+# ABOVE AND BELOW ARE SAME
+
+# time:O(nlogn), space:O(n) - NEETCODE
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        # reverse sorted
+        pair = sorted([[position[i], speed[i]] for i in range(len(position))])[::-1]
+
+        stack = []
+
+        for p,s in pair:
+            stack.append((target-p)/s)
+            # collision - we start with greatest position car and then second greatest  
+            if len(stack) >=2 and stack[-1] <= stack[-2]:
+                stack.pop()
+
+        return len(stack)
+
