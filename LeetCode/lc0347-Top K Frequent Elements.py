ef3b33150dcee3661d901c94d685c5cda53fab48
Rahul Bakshee <rhlbks@gmail.com>
2024-10-03 11:39:05 -0700

    Update lc0347-Top K Frequent Elements.py

--- LeetCode/lc0347-Top K Frequent Elements.py
+++ LeetCode/lc0347-Top K Frequent Elements.py
@@ -1,46 +1,30 @@
-# good solutions at  - https://leetcode.com/problems/top-k-frequent-elements/solutions/1502514/c-python-2-solutions-maxheap-bucket-sort-clean-concise/
-
-
-# using heap - 
-# time:O(n) freq + O(n) heapify best OR O(nlogn) heapify worst case + O(klogn) heappop
-# space:O(n) freq, + O(n) heap
+# time:O(nlogK)
+# space:O(n)
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = {}
-        for num in nums:
-            if num in freq:
-                freq[num] += 1
-            else:
-                freq[num] = 1
+        # create num:freq mapping
+        counter = Counter(nums)
 
-        # freq: key:num, val:occurances {1:3, 2:5, 7:1}
-
-        # maxHeap for storing the (occurances, num)
-
-        maxHeap = [(-1*occur, key) for key, occur in freq.items()]
-        heapify(maxHeap)
-
+        # craete minHeap
+        minHeap = []
+        for key, val in counter.items():
+            heapq.heappush(minHeap, (val, key))
+            if len(minHeap) > k:
+                heapq.heappop(minHeap)
 
         result = []
-        for _ in range(k):
-            occur, key = heappop(maxHeap)
+        for val, key in minHeap:
             result.append(key)
 
         return result
 
-
-
+        
 
 # bucket sort - 
 # time-O(n), space-O(n)
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        for num in nums:
-            if num in count:
-                count[num] +=1
-            else:
-                count[num] = 1
+        count = Counter(nums)
 
         freq = [[] for i in range(len(nums)+1)]
         for n, c in count.items():
