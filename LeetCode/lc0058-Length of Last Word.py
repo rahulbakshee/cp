e5f5a6f0205de0998ec2cf7cf8ba452eb0e2be19
Rahul Bakshee <rhlbks@gmail.com>
2024-10-04 13:57:45 -0700

    Create lc0058-Length of Last Word.py

---
+++ LeetCode/lc0058-Length of Last Word.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        return len(s.split()[-1])
+
+
+
+
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        length = 0
+        for i in range(len(s)-1, -1, -1):
+            if s[i] == " ":
+                if length != 0:
+                    return length
+            else:
+                length += 1
+        return length
