a52c7fd4609a1802da52db6f7c903fe60559a8fb
Rahul Bakshee <rhlbks@gmail.com>
2024-10-07 14:22:52 -0700

    Update lc0374-Guess Number Higher or Lower.py

--- LeetCode/lc0374-Guess Number Higher or Lower.py
+++ LeetCode/lc0374-Guess Number Higher or Lower.py
@@ -21,17 +21,17 @@
 class Solution:
     def guessNumber(self, n: int) -> int:
         left, right = 1, n
-
         while left < right:
             mid = left + (right-left)//2
-
-            if guess(mid) == 0:
-                return mid
-            elif guess(mid) == -1:
-                right = mid
+            # higher
+            if guess(mid) == -1:
+                right = mid-1
+            # lower
             elif guess(mid) == 1:
-                left = mid + 1
+                left = mid+1
+            # equal
             else:
-                pass
-        return left
+                return mid
 
+        return left
+                
