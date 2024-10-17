3b56b2f0e0eef66bd95c7542555b1f6d61d67d55
Rahul Bakshee <rhlbks@gmail.com>
2024-10-17 16:03:16 -0700

    Update lc2848-Points That Intersect With Cars.py

--- LeetCode/lc2848-Points That Intersect With Cars.py
+++ LeetCode/lc2848-Points That Intersect With Cars.py
@@ -1,17 +1,25 @@
 # time:O(nlogn), space:O(n)
 class Solution:
     def numberOfPoints(self, nums: List[List[int]]) -> int:
+        # sort the input array
         nums.sort()
+
+        # see the result array with first interval
         result = [nums[0]]
+
         count = 0
 
+        # pass 1- check the newer range with the exsting range
         for i in range(1, len(nums)):
+            # overlapping
             if nums[i][0] <= result[-1][1]:
                 result[-1][0] = min(result[-1][0], nums[i][0])
                 result[-1][1] = max(result[-1][1], nums[i][1])
             else:
+                # not overlapping
                 result.append(nums[i])
 
+        # pass 2- to count the points in the ranges
         for res in result:
             count += res[1] - res[0] + 1
         return count 
