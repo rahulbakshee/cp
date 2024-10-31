f5448a4fd2e3b71c04d4d137a54e6f4d73c3681e
Rahul Bakshee <rhlbks@gmail.com>
2024-10-31 10:51:54 -0700

    Update lc0295-Find Median from Data Stream.py

--- LeetCode/lc0295-Find Median from Data Stream.py
+++ LeetCode/lc0295-Find Median from Data Stream.py
@@ -1,4 +1,30 @@
-# time:O()
+# sorting - time:O(m*nlogn)
+"""
+addNum - just append at the end of array - time:O(1)
+findMedian - sort the array - O(nlogn) n elemnets in array
+           - find the middle element(s)
+           - overall O(m*nlogn) m is the number of times "findMedian" func is called
+"""
+
+class MedianFinder:
+    def __init__(self):
+        self.arr = []
+
+    def addNum(self, num:int)->None:
+        """adds the integer num from the data stream to the data structure."""
+        self.arr.append(num)
+
+    def findMedian(self)->float:
+        """ returns the median of all elements so far"""
+        self.arr.sort()
+        n = len(self.arr)
+
+        if n%2:
+            return self.arr[n//2]
+        else:
+            return (self.arr[n//2-1] + self.arr[n//2]) * 0.5
+
+
 
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
