9c9d953bf3b68cedb7d5e5b9d1090fbc474f1dcb
Rahul Bakshee <rhlbks@gmail.com>
2024-11-09 10:19:43 -0800

    Update lc0075-Sort Colors.py

--- LeetCode/lc0075-Sort Colors.py
+++ LeetCode/lc0075-Sort Colors.py
@@ -18,15 +18,30 @@ class Solution:
 
 
         # 3-optimal - three pointers - time:O(n), space:O(1)
+        
+        # --------------
+        # | 0  |  1 |  2|
+        # everything between 0 and (low-1) - all 0
+        # everything between low and (mid-1) - all 1
+        # everything between mid and high - all unsorted
+        # everything between high+1 and (n-1) - all 2
+        # 
         low, mid, high = 0,0,len(nums)-1
-
         while mid <= high:
             if nums[mid] == 0:
+                # swap between nums[low] and nums[mid]
+                # increase low and mid
                 nums[mid], nums[low] = nums[low], nums[mid]
                 low += 1
                 mid += 1
             elif nums[mid] == 1:
+                # no swapping
+                # increase mid
                 mid += 1
             else:
+                # swap between nums[mid] and nums[high]
+                # increase mid and decrease high
                 nums[mid], nums[high] = nums[high], nums[mid]
                 high -= 1
+
+                
