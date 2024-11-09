4ebf21a0972093e6ec20aaddc31c5a1f82e46949
Rahul Bakshee <rhlbks@gmail.com>
2024-11-09 15:53:19 -0800

    Update lc0134-Gas Station.py

--- LeetCode/lc0134-Gas Station.py
+++ LeetCode/lc0134-Gas Station.py
@@ -1,23 +1,16 @@
 # time:O(n), space:O(1)
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        
+        if sum(gas) < sum(cost):
+            return -1
 
-        # gas =  [1,2, 3, 4, 5]
-        # cost = [3,4, 5, 1, 2]
-        # remain=[-2,-2,-2,3,3]
- 
-        total_gain = 0
-        curr_gain = 0
+        total = 0
         index = 0
-
         for i in range(len(gas)):
-            total_gain += gas[i] - cost[i]
-            curr_gain += gas[i] - cost[i]
+            total += gas[i] - cost[i]
 
-            # if it is a valley
-            if curr_gain < 0:
-                curr_gain = 0
+            if total < 0:
+                total = 0
                 index = i+1
 
-        return index if total_gain >=0 else -1
+        return index
