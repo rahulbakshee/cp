c235df91fcb3cf739672ebf45e05138e1cd58b9d
Rahul Bakshee <rhlbks@gmail.com>
2024-12-04 17:23:33 -0800

    Update lc0334-Increasing Triplet Subsequence.py

--- LeetCode/lc0334-Increasing Triplet Subsequence.py
+++ LeetCode/lc0334-Increasing Triplet Subsequence.py
@@ -10,3 +10,17 @@ class Solution:
         return False
 
 
+# time:O(n), space:O(1)
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        first = float("inf")
+        second = float("inf")
+
+        for num in nums:
+            if num <= first:
+                first = num
+            elif num <= second:
+                second = num
+            else:
+                return True
+        return False
