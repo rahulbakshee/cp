e7f9d392ec03de69709f8cf390183c6ae8be462d
Rahul Bakshee <rhlbks@gmail.com>
2024-12-06 10:04:48 -0800

    Update lc1679-Max Number of K-Sum Pairs.py

--- LeetCode/lc1679-Max Number of K-Sum Pairs.py
+++ LeetCode/lc1679-Max Number of K-Sum Pairs.py
@@ -1,3 +1,23 @@
+# nested loops, time:O(n^2), space:O(1)
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] == -1:
+                continue
+            for j in range(i+1, len(nums)):
+                if nums[j] == -1:
+                    continue
+                if nums[i] + nums[j] == k:
+                    count += 1
+                    nums[i] = -1
+                    nums[j] = -1
+                    break
+
+        return count
+
+
+
 # sorting + two poinmters
 # time:O(nlogn), space:O(sorting)
 class Solution:
