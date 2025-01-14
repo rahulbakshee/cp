e8db883672e412efdaa52c7ac4a38b3a29f80cdd
Rahul Bakshee <rhlbks@gmail.com>
2025-01-13 23:01:59 -0800

    Update lc0394-Decode String.py

--- LeetCode/lc0394-Decode String.py
+++ LeetCode/lc0394-Decode String.py
@@ -25,3 +25,32 @@ class Solution:
             i += 1
 
         return curr_str
+
+
+# time:O(n), space:O(n)
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+
+        for char in s:
+            if char != "]":
+                stack.append(char)
+
+            else:
+                curr_str = ""
+                # keep on popping from stack until "["
+                while stack[-1] != "[":
+                    curr_str = stack.pop() + curr_str
+
+                # pop to remove "["
+                stack.pop()
+
+                # try to get the number
+                curr_num = ""
+                while stack and stack[-1].isdigit():
+                    curr_num = stack.pop() + curr_num
+
+                curr_str = int(curr_num) * curr_str
+                stack.append(curr_str)
+
+        return "".join(stack)
