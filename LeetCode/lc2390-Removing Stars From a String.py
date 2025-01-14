8ac1b289316d7f3f95207c0e93e868facc86fe42
Rahul Bakshee <rhlbks@gmail.com>
2025-01-13 20:58:30 -0800

    Update lc2390-Removing Stars From a String.py

--- LeetCode/lc2390-Removing Stars From a String.py
+++ LeetCode/lc2390-Removing Stars From a String.py
@@ -8,3 +8,25 @@ class Solution:
                 stack.append(char)
 
         return "".join(stack)
+
+
+
+# time:O(n), space:O(1)
+class Solution:
+    def removeStars(self, s: str) -> str:
+        result = []
+
+        i,j = 0,0
+        while j<len(s):
+            if s[j] == "*":
+                if result:
+                    result.pop()
+                
+            else:
+                result.append(s[j])
+
+            
+            j += 1
+            i = j
+
+        return "".join(result)
