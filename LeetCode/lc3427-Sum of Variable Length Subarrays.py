c9efea2d170ec3336aac85625854de74b7cbd5d2
Rahul Bakshee <rhlbks@gmail.com>
2025-01-19 14:58:39 -0800

    Create lc3427-Sum of Variable Length Subarrays.py

---
+++ LeetCode/lc3427-Sum of Variable Length Subarrays.py
@@ -0,0 +1,8 @@
+# time:O(n), space:O(1)
+class Solution:
+    def subarraySum(self, nums: List[int]) -> int:
+        result = 0
+        for i in range(len(nums)):
+            start = max(0, i-nums[i])
+            result += sum(nums[start:i+1])
+        return result
