15a9d20c52d9b47a575c66720cf6549c36d8233d
Rahul Bakshee <rhlbks@gmail.com>
2025-01-22 16:31:19 -0800

    Update lc2852-Sum of Remoteness of All Cells.py

--- LeetCode/lc2852-Sum of Remoteness of All Cells.py
+++ LeetCode/lc2852-Sum of Remoteness of All Cells.py
@@ -44,3 +44,49 @@ class Solution:
 
 
         return result
+
+
+# using BFS-iterative using a Queue
+# time:O(n^2), space:O(n^2)
+class Solution:
+    def sumRemoteness(self, grid: List[List[int]]) -> int:
+        # step3 - define the bfs
+        def bfs(i,j,total_sum):
+            comp_sum = grid[i][j]
+            comp_size = 1
+            grid[i][j] = -1
+
+            q = deque([(i,j,)])
+            while q:
+                curr_row, curr_col = q.popleft()
+
+                # explore all dirs
+                dirs = [(0,-1), (0,1), (-1,0), (1,0)]
+                for dx,dy in dirs:
+                    new_row = curr_row + dx
+                    new_col = curr_col + dy
+                    # check if new coordinate is valid
+                    if 0<=new_row<len(grid) and 0<=new_col<len(grid[0]) and grid[new_row][new_col] != -1:
+                        q.append((new_row, new_col))
+                        comp_sum += grid[new_row][new_col]
+                        comp_size += 1
+                        grid[new_row][new_col] = -1
+
+            return (total_sum - comp_sum) * comp_size
+
+
+        # step1 - calculate total_sum of all the cells
+        total_sum = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] != -1:
+                    total_sum += grid[i][j]
+
+        # step2 - calculate remoteness 
+        result = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] != -1:
+                    result += bfs(i,j,total_sum)
+
+        return result
