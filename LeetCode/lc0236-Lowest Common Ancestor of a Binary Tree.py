2a407fa04793d5b3e4b108227aee5b1736de9c31
Rahul Bakshee <rhlbks@gmail.com>
2025-01-23 22:44:32 -0800

    Update lc0236-Lowest Common Ancestor of a Binary Tree.py

--- LeetCode/lc0236-Lowest Common Ancestor of a Binary Tree.py
+++ LeetCode/lc0236-Lowest Common Ancestor of a Binary Tree.py
@@ -5,7 +5,7 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
+# time:O(n), space:O(n)
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         if not root:
@@ -21,3 +21,42 @@ class Solution:
             return root
         else:
             return left or right
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def backtrack(node):
+            nonlocal result
+            if not node:
+                return False
+
+            left = backtrack(node.left)
+            right = backtrack(node.right)
+            if node == p or node == q:
+                mid = True
+            else:
+                mid = False
+
+            if left+mid+right>=2:
+                result = node
+
+            return left or mid or right
+
+        result = None
+        backtrack(root)
+        return result
+
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return 
+
+        left_result = self.lowestCommonAncestor(root.left, p, q)
+        right_result = self.lowestCommonAncestor(root.right, p, q)
+
+        if (left_result and right_result) or (root in (p,q)):
+            return root
+        else:
+            return left_result or right_result
