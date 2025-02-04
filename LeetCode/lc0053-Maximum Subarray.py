e5a2863a0625941ed266e514727c1fc929a60519
Rahul Bakshee <rhlbks@gmail.com>
2025-02-03 21:12:14 -0800

    Create lc0053-Maximum Subarray.py

---
+++ LeetCode/lc0053-Maximum Subarray.py
@@ -0,0 +1,24 @@
+# time:O(n^2), space:O(1)
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum = float("-inf")
+        for i in range(len(nums)):
+            curr_sum = 0
+            for j in range(i, len(nums)):
+                curr_sum += nums[j]
+                max_sum = max(max_sum, curr_sum)
+
+        return max_sum
+
+
+# kadane algo
+# time:O(n), space:O(1)
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum = float("-inf")
+        curr_sum = 0
+
+        for num in nums:
+            curr_sum = max(num, curr_sum + num)
+            max_sum = max(max_sum, curr_sum)
+        return max_sum
