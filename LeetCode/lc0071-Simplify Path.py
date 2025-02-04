e1bdecf4e953f11c53653613bb68bdc96a68dc99
Rahul Bakshee <rhlbks@gmail.com>
2025-02-03 16:45:26 -0800

    Update lc0071-Simplify Path.py

--- LeetCode/lc0071-Simplify Path.py
+++ LeetCode/lc0071-Simplify Path.py
@@ -16,3 +16,32 @@ class Solution:
                 stack.append(item)            
                 
         return "/" + "/".join(stack) if stack else "/"
+
+
+
+# https://leetcode.com/discuss/interview-question/6161836/Meta's-Variant-for-Leetcode-71-(Simplify-Path)/
+
+def get_path(cwd, cd):
+    
+    def simplify(path):
+        sub_parts = path.split("/")
+        result = []
+        for part in sub_parts:
+            if part == "." or part == "":
+                continue
+            elif part == "..":
+                if result: result.pop()
+            else:
+                result.append(part)
+        answer = "/".join(result)
+        return "/" + answer
+
+    
+    if not cd:                  # Case 0
+        return simplify(cwd)
+    if not cwd:                 # Case 0
+        return simplify(cd)
+    if cd.startswith("/"):      # Case 1
+        return simplify(cd)
+    else:                       # Case 2
+        return simplify(cwd + "/" + cd)
