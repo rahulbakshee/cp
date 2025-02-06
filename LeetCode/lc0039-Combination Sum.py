dbc6f71066d2474fd695375be1df2f0355555a37
Rahul Bakshee <rhlbks@gmail.com>
2025-02-06 13:05:38 -0800

    Update lc0039-Combination Sum.py

--- LeetCode/lc0039-Combination Sum.py
+++ LeetCode/lc0039-Combination Sum.py
@@ -1,3 +1,34 @@
+# time:O(n^t) roughly. n-len of candidates, t-target
+# space:O(t)
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        def backtrack(i, curr_sum, sol):
+            # base case
+            # success condition
+            if curr_sum == target:
+                result.append(sol.copy())
+                return
+
+            if curr_sum > target or i>=len(candidates):
+                return 
+
+            # take it
+            sol.append(candidates[i])
+            backtrack(i, curr_sum + candidates[i], sol)
+
+            # don't take it
+            sol.pop()
+            backtrack(i+1, curr_sum, sol)
+            
+        
+        
+        result = []
+        backtrack(0, 0, [])
+        return result
+
+
+
+
 # time: roughly O(n**t)
 # space:O(n) - callstack
 class Solution:
