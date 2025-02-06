6b49c45ff0a8ce4972a3a49d4be02c8888b5d273
Rahul Bakshee <rhlbks@gmail.com>
2025-02-06 12:12:36 -0800

    Update lc0079-Word Search.py

--- LeetCode/lc0079-Word Search.py
+++ LeetCode/lc0079-Word Search.py
@@ -1,3 +1,41 @@
+# time:O(n.3^l) - n -number of cells, l-max len of word to be matched
+# space:O(l)
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        def backtrack(row, col, suffix):
+            # base case
+            if len(suffix) == 0:
+                return True
+            
+            # check for boundry
+            if row<0 or row>= len(board) or col<0 or col>=len(board[0]) or board[row][col]!=suffix[0]:
+                return False
+
+            # first step of backtrack - mark the current state with #
+            board[row][col] = "#"
+
+            # explore the neighboring directions
+            for dr, dc in [(0,1),(1,0),(-1,0),(0,-1)]:
+                new_row = row+dr
+                new_col = col+dc
+                if backtrack(new_row, new_col, suffix[1:]):
+                    return True
+
+            # second step of backtrack - unmark "#" and assign the char as is to the grid
+            board[row][col] = suffix[0]
+            return False
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if backtrack(i,j,word):
+                    return True
+        
+        return False
+
+
+
+
+
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         rows = len(board)
