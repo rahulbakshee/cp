6b6293af2a658a468612c5175d2d28f355a87690
Rahul Bakshee <rhlbks@gmail.com>
2025-02-08 23:42:34 -0800

    Update lc0215-Kth Largest Element in an Array.py

--- LeetCode/lc0215-Kth Largest Element in an Array.py
+++ LeetCode/lc0215-Kth Largest Element in an Array.py
@@ -19,3 +19,29 @@ class Solution:
             if len(minHeap) > k:
                 heapq.heappop(minHeap)
         return heapq.heappop(minHeap)
+
+
+# Time complexity: O(n) on average, O(n^2) in the worst case
+# space:O(n)
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        def quickSelect(nums, k):
+            pivot = random.choice(nums)
+            left, mid, right = [], [], []
+
+            for num in nums:
+                if num > pivot:
+                    left.append(num)
+                elif num < pivot:
+                    right.append(num)
+                else:
+                    mid.append(num)
+
+            if k <= len(left):
+                return quickSelect(left, k)
+            
+            if len(left)+len(mid) < k:
+                return quickSelect(right, k-len(left)-len(mid))
+            
+            return pivot
+        return quickSelect(nums, k)
