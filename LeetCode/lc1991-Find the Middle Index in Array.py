b7c43f18f4103a68915ba8f44adfa43cbe07bb7b
Rahul Bakshee <rhlbks@gmail.com>
2025-02-09 14:23:43 -0800

    Update lc1991-Find the Middle Index in Array.py

--- LeetCode/lc1991-Find the Middle Index in Array.py
+++ LeetCode/lc1991-Find the Middle Index in Array.py
@@ -1,9 +1,24 @@
 # https://leetcode.com/problems/find-the-middle-index-in-array
 
-# space: O(1), time: O(n)
+# time:O(n^2), space: O(1)
 class Solution:
     def findMiddleIndex(self, nums: List[int]) -> int:
         for i in range(len(nums)):
             if sum(nums[:i]) == sum(nums[i+1:]):
                 return i
+        return -1
+
+
+# time:O(n), space:O(1)
+class Solution:
+    def findMiddleIndex(self, nums: List[int]) -> int:
+        left_sum = 0
+        total_sum = sum(nums)
+
+        for i in range(len(nums)):
+            right_sum = total_sum - left_sum - nums[i]
+            if left_sum == right_sum:
+                return i
+            left_sum += nums[i]
+
         return -1
