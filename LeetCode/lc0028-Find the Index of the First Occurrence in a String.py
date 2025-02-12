2c23f2508463db84281ae920bc94cf6988ec21c6
Rahul Bakshee <rhlbks@gmail.com>
2025-02-12 12:15:49 -0800

    Update lc0028-Find the Index of the First Occurrence in a String.py

--- LeetCode/lc0028-Find the Index of the First Occurrence in a String.py
+++ LeetCode/lc0028-Find the Index of the First Occurrence in a String.py
@@ -15,3 +15,35 @@ class Solution:
                     return i
 
         return -1
+
+
+
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if haystack == needle:
+            return 0
+        nee = len(needle)
+        hay = len(haystack)
+        
+        for i in range(hay):
+            if haystack[i:i+nee] == needle:
+                return i
+            
+        return -1
+            
+# time:O(needle_len*haystack_len), space:O(1)
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        needle_len = len(needle)
+        haystack_len = len(haystack)
+
+        for start in range(haystack_len-needle_len+1):
+            for needle_index in range(needle_len):
+                if haystack[start+needle_index] != needle[needle_index]:
+                    break
+
+                if needle_index == needle_len - 1:
+                    return start
+
+        return -1
