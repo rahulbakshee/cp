f424334da046957c2034ba321cd289886506338e
Rahul Bakshee <rhlbks@gmail.com>
2025-02-14 14:21:33 -0800

    Update lc0286-Walls-and-Gates.py

--- LeetCode/lc0286-Walls-and-Gates.py
+++ LeetCode/lc0286-Walls-and-Gates.py
@@ -3,45 +3,50 @@
 # trick is to start from the gate(grid[i][j] == 0
 
 
-# bfs - queue
-import collections
+# time:O(mn), space:O(mn)
+from collections import deque
+
 class Solution:
-    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
-        rows = len(grid)
-        cols = len(grid[0])
-
+    def wallsAndGates(self, rooms: List[List[int]]) -> None:
+        """
+        Do not return anything, modify rooms in-place instead.
+        """
+        # 0 - measure the dimensions of the matrix
+        rows = len(rooms)
+        cols = len(rooms[0])
+        q = deque()
+        directions = [(0,1), (1,0), (0,-1), (-1,0)]
         visited = set()
-        q = collections.deque()
 
-        # add all gates to the queue
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == 0:
-                    q.append((i,j))
-                    visited.add((i,j))
+        # 1 - add all the gates to the queue
+        for r in range(rows):
+            for c in range(cols):
+                if rooms[r][c] == 0:
+                    q.append((r,c))
+                    visited.add((r,c))
 
-        # def bfs
-        def addRoom(r,c):
-            if (r<0 or r>=rows or c<0 or c>=cols
-                or (r,c) in visited
-                or grid[r][c] == -1):
-                return 
-            visited.add((r,c))
-            q.append((r,c))
-            
-
-        # call bfs
+        
+        # 2 - BFS
         distance = 0
         while q:
             for i in range(len(q)):
-                r,c = q.popleft()
-                grid[r][c] = distance
-                addRoom(r+1,c)
-                addRoom(r,c+1)
-                addRoom(r-1,c)
-                addRoom(r,c-1)
+                r, c = q.popleft()
+                # mark the distance
+                rooms[r][c] = distance
 
+                # explore neighbors
+                for direction in directions:
+                    dr, dc = direction
+                    new_r = r + dr
+                    new_c = c + dc
+
+                    # check for boundry, not in visited, obstacle
+                    if not (new_r<0 or new_r>=rows or
+                            new_c<0 or new_c>=cols or
+                            (new_r, new_c) in visited or
+                            rooms[new_r][new_c] in [-1,0]):
+                            
+                            q.append((new_r, new_c))
+                            visited.add((new_r, new_c))
 
             distance += 1
-
-        
