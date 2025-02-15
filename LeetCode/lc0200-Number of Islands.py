c17bb82ca305ed8da84ea47b177d926d5aa7c933
Rahul Bakshee <rhlbks@gmail.com>
2025-02-14 16:09:58 -0800

    Update lc0200-Number of Islands.py

--- LeetCode/lc0200-Number of Islands.py
+++ LeetCode/lc0200-Number of Islands.py
@@ -4,31 +4,49 @@
 # bfs - with popleft and dfs with pop
 
 from collections import deque
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        
+        # 0 - dimensions
+        rows = len(grid)
+        cols = len(grid[0])
         visited = set()
-        islands = 0
-
+        result = 0
+        directions = [(0,1), (1,0), (-1,0), (0,-1)]
+        
+        
+        # 2 - define BFS
         def bfs(r,c):
             q = deque()
             q.append((r,c))
             visited.add((r,c))
-
+            
             while q:
-                row, col = q.popleft()
-                for x, y in [(row+1,col),(row-1,col),(row,col+1),(row,col-1)]:
-                    if 0<=x<len(grid) and 0<=y<len(grid[0]) and (x,y) not in visited and grid[x][y] == "1":
-                        q.append((x,y))
-                        visited.add((x,y))
+                r,c= q.pop()
+                for dr, dc in directions:
+                    new_r = r+dr
+                    new_c = c+dc
+                    # check for boundary
+                    if not (new_r<0 or new_r>=rows or new_c<0 or new_c>=cols or
+                            (new_r,new_c) in visited or 
+                            grid[new_r][new_c] == "0"):
+                        q.append((new_r,new_c))
+                        visited.add((new_r,new_c))
+            
 
+        # 1 - call BFS
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r,c) not in visited:
+                    # call bfs on that cell
+                    bfs(r,c)
+                    result += 1
+                    
+        return result
 
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == "1" and (i,j) not in visited:
-                    bfs(i,j)
-                    islands += 1
-
-        return islands
 
 
 
