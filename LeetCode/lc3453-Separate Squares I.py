4c67ecde7dffb14f3023da6d2d697cc64eed3667
Rahul Bakshee <rhlbks@gmail.com>
2025-02-15 09:47:02 -0800

    Create lc3453-Separate Squares I.py

---
+++ LeetCode/lc3453-Separate Squares I.py
@@ -0,0 +1,39 @@
+class Solution:
+    def separateSquares(self, squares: List[List[int]]) -> float:
+        
+        def helper(point):
+            aabove, abelow = 0,0
+
+            for x,y,l in squares:
+                total = l*l
+
+                if point <= y:
+                    aabove += total
+                elif point >=y+l:
+                    abelow += total
+                else:
+                    aheight = y+l-point
+                    bheight = point -y
+                    aabove += l * aheight
+                    abelow += l* bheight
+            return aabove - abelow
+                
+
+
+        low = 0
+        high = 10**10
+        iterations = 0
+
+        while high-low >= pow(10, -5):
+            mid = (low+high)/2
+
+            diff = helper(mid)
+
+            if diff <=0:
+                high = mid
+            else:
+                low = mid
+
+            iterations += 1
+        print(iterations)
+        return high
