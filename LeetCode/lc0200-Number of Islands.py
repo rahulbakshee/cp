88141e8d6c8db8d0eb4875490286b7e77acd19e7
Rahul Bakshee <rhlbks@gmail.com>
2025-02-17 13:57:36 -0800

    Update lc0200-Number of Islands.py

--- LeetCode/lc0200-Number of Islands.py
+++ LeetCode/lc0200-Number of Islands.py
@@ -1,80 +1,148 @@
-# REFER NEETCODE VIDEO EXPLANATION
-# for both DFS and BFS
+#############################################################
+# my solutions below
 
-# bfs - with popleft and dfs with pop
+# DFS using recursion
 
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(r,c):
+            # base case
+            if r<0 or r>=rows or c<0 or c>=cols:
+                return
+            if (r,c) in visited:
+                return
+            if grid[r][c] == "0":
+                return
+            
+            visited.add((r,c))
+            for dr, dc in directions:
+                new_row = r+dr
+                new_col = c+dc
+                dfs(new_row, new_col)  
+        
+        
+        if not grid:
+            return 0
+        islands = 0
+        rows = len(grid)
+        cols = len(grid[0])
+        visited = set()
+        directions = [(0,1),(1,0),(0,-1),(-1,0)]
+        
+        for r in range(rows):
+            for c in range(cols):
+                if (r,c) not in visited and grid[r][c] == "1":
+                    dfs(r,c)
+                    islands += 1
+        
+        return islands
+    
+
+# BFS using QUEUE
 from collections import deque
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-        
-        # 0 - dimensions
-        rows = len(grid)
-        cols = len(grid[0])
-        visited = set()
-        result = 0
-        directions = [(0,1), (1,0), (-1,0), (0,-1)]
-        
-        
-        # 2 - define BFS
         def bfs(r,c):
             q = deque()
             q.append((r,c))
             visited.add((r,c))
             
             while q:
-                r,c= q.pop()
-                for dr, dc in directions:
-                    new_r = r+dr
-                    new_c = c+dc
+                row,col = q.popleft()
+                visited.add((row,col))
+                
+                # explore neighbors
+                for dr,dc in directions:
+                    new_row = row+dr
+                    new_col = col+dc
+                    
                     # check for boundary
-                    if not (new_r<0 or new_r>=rows or new_c<0 or new_c>=cols or
-                            (new_r,new_c) in visited or 
-                            grid[new_r][new_c] == "0"):
-                        q.append((new_r,new_c))
-                        visited.add((new_r,new_c))
-            
-
-        # 1 - call BFS
+                    if new_row<0 or new_row==rows or new_col<0 or new_col==cols:
+                        continue
+                    
+                    # check if not in visited
+                    if (new_row, new_col) in visited:
+                        continue
+                    
+                    # check if land vs. water
+                    if grid[new_row][new_col] == "0":
+                        continue
+                        
+                    # explore the neighbors by adding them to the stack
+                    if (new_row,new_col) not in q:
+                        q.append((new_row, new_col))
+        
+        
+        if not grid:
+            return 0
+        
+        rows = len(grid)
+        cols = len(grid[0])
+        visited = set()
+        islands = 0
+        directions = [(0,1), (1,0), (-1,0), (0,-1)]
+        
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1" and (r,c) not in visited:
-                    # call bfs on that cell
                     bfs(r,c)
-                    result += 1
+                    islands += 1
                     
-        return result
+        return islands
 
 
 
 
-# simple DFS - 
+# DFS using STACK
 from collections import deque
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        def bfs(r,c):
+            q = deque()
+            q.append((r,c))
+            visited.add((r,c))
+            
+            while q:
+                row,col = q.pop()
+                visited.add((row,col))
+                
+                # explore neighbors
+                for dr,dc in directions:
+                    new_row = row+dr
+                    new_col = col+dc
+                    
+                    # check for boundary
+                    if new_row<0 or new_row==rows or new_col<0 or new_col==cols:
+                        continue
+                    
+                    # check if not in visited
+                    if (new_row, new_col) in visited:
+                        continue
+                    
+                    # check if land vs. water
+                    if grid[new_row][new_col] == "0":
+                        continue
+                        
+                    # explore the neighbors by adding them to the stack
+                    if (new_row,new_col) not in q:
+                        q.append((new_row, new_col))
+        
+        
+        if not grid:
+            return 0
+        
+        rows = len(grid)
+        cols = len(grid[0])
         visited = set()
         islands = 0
-
-
-        def dfs(r,c):
-            if r<0 or r>=len(grid) or c<0 or c>=len(grid[0]) or (r,c) in visited or grid[r][c] !="1":
-                return
-            
-            visited.add((r,c))
-
-            dfs(r,c+1)
-            dfs(r,c-1)
-            dfs(r+1,c)
-            dfs(r-1,c)
-            return 
-
-
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == "1" and (i,j) not in visited:
-                    dfs(i,j)
+        directions = [(0,1), (1,0), (-1,0), (0,-1)]
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r,c) not in visited:
+                    bfs(r,c)
                     islands += 1
-
+                    
         return islands
