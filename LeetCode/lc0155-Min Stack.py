051ef6393242d7cea5d71a88ba03c0da6d84109c
Rahul Bakshee <rhlbks@gmail.com>
2025-02-17 10:06:26 -0800

    Update lc0155-Min Stack.py

--- LeetCode/lc0155-Min Stack.py
+++ LeetCode/lc0155-Min Stack.py
@@ -1,18 +1,17 @@
-# https://leetcode.com/problems/min-stack/description/
-
 class MinStack:
 
     def __init__(self):
         self.stack = []
         self.minStack = []
+        
 
     def push(self, val: int) -> None:
         self.stack.append(val)
+        
         if self.minStack:
-            self.minStack.append(min(val, self.minStack[-1]))
-        else:
-            self.minStack.append(val)
-
+            val = min(val, self.minStack[-1])
+        self.minStack.append(val)
+        
     def pop(self) -> None:
         self.stack.pop()
         self.minStack.pop()
