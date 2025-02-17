444a73738801328ae70059a3e0bfc2a1f15f026b
Rahul Bakshee <rhlbks@gmail.com>
2025-02-17 11:13:23 -0800

    Update lc0150-Evaluate Reverse Polish Notation.py

--- LeetCode/lc0150-Evaluate Reverse Polish Notation.py
+++ LeetCode/lc0150-Evaluate Reverse Polish Notation.py
@@ -16,3 +16,29 @@ class Solution:
                 stack.append(int(c))
 
         return stack.pop()
+
+
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        
+        for token in tokens:
+            if token in ['+', '-', '*', '/']:
+                b = int(stack.pop())
+                a = int(stack.pop())
+                
+                if token == "+":
+                    stack.append(a+b)
+                elif token == "-":
+                    stack.append(a-b)
+                elif token == "/":
+                    stack.append(int(a/b))
+                elif token == "*":
+                    stack.append(a*b)
+                
+            else:
+                stack.append(int(token))
+                
+        return stack.pop()
