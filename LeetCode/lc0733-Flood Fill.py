bf9391b0f48860ac95689cf8e61ceccc370692a6
Rahul Bakshee <rhlbks@gmail.com>
2025-02-19 10:53:32 -0800

    Update lc0733-Flood Fill.py

--- LeetCode/lc0733-Flood Fill.py
+++ LeetCode/lc0733-Flood Fill.py
@@ -1,3 +1,80 @@
+# DFS recursion  - I solved my self on 02/19/2025
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        
+        def dfs(sr,sc):
+            # base case - boundaries, color of pizel in image
+            if sr<0 or sr>=rows or sc<0 or sc>=cols or image[sr][sc] == color:
+                return
+            
+            # start processing if the curr color is accepted_color
+            if image[sr][sc] == accepted_color:
+
+                # change the  color of curr pixel
+                image[sr][sc] = color
+
+                # start exploring the neighbors of curr pizel
+                for dr, dc in directions:
+                    new_sr = sr+dr
+                    new_sc = sc+dc
+
+                    dfs(new_sr, new_sc)        
+
+        
+        if not image:
+            return []
+        
+        rows = len(image)
+        cols = len(image[0])
+        directions = [(0,1),(1,0),(-1,0),(0,-1)]
+        accepted_color = image[sr][sc]
+        
+        dfs(sr,sc)
+        return image
+
+# DFS with stack
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        if not image:
+            return []
+        
+        rows = len(image)
+        cols = len(image[0])
+        
+        directions = [(0,1), (1,0), (0,-1), (-1,0)]
+        
+        starting_color = image[sr][sc]
+        
+        stack = [[sr,sc]]
+        if starting_color!= color:
+            while stack:
+                row, col = stack.pop()
+                # check for bounadries
+                if row<0 or row>=rows or col<0 or col>=cols:
+                    continue
+                
+                # check for current color == starting color
+                if image[row][col] == starting_color:
+                    
+                    # change the color of current cell
+                    image[row][col] = color
+                    
+                    # start exploring the neighbors
+                    for dr, dc in directions:
+                        new_row = row+dr 
+                        new_col = col+dc
+                        
+                        stack.append([new_row, new_col])             
+                    
+        return image
+        
+
+
+
+
+###########################################################################################################
+
 # dfs - time:O(m*n), space:O(m*n) for dfs call stack
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
