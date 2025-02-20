a9c1373d90e9233b644abb09412f816e9368e54e
Rahul Bakshee <rhlbks@gmail.com>
2025-02-20 11:33:40 -0800

    Update lc0098-Validate Binary Search Tree.py

--- LeetCode/lc0098-Validate Binary Search Tree.py
+++ LeetCode/lc0098-Validate Binary Search Tree.py
@@ -4,19 +4,66 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# time:O(n), space:O(n)
+
+# DFS recursion
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        
-        def valid(node, left, right):
+        def dfs(node, left_bound, right_bound):
             # base case
             if not node:
                 return True
 
-            if not (node.val < right and node.val > left):
+            # check for boundariues (of BST)
+            if not (left_bound<node.val<right_bound):
                 return False
-            
-            return valid(node.left, left, node.val) and valid(node.right, node.val, right)
 
+            return dfs(node.left, left_bound, node.val) and dfs(node.right, node.val, right_bound)
 
-        return valid(root, float("-inf"), float("inf"))
-            
+        if not root:
+            return True
+        
+        return dfs(root, float("-inf"), float("inf"))
+
+# DFS stack
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+
+        stack = [[root, float(-inf), float("inf")]]
+        while stack:
+            node, left_bound, right_bound = stack.pop()
+
+            if node:
+                if not (left_bound < node.val < right_bound):
+                    return False
+
+                    
+                stack.append([node.left, left_bound, node.val])
+                stack.append([node.right, node.val, right_bound])
+
+        return True
+
+# BFS queue
+from collections import deque
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+
+        q = deque([[root, float(-inf), float("inf")]])
+        while q:
+            node, left_bound, right_bound = q.popleft()
+
+            if node:
+                if not (left_bound < node.val < right_bound):
+                    return False
+
+                    
+                q.append([node.left, left_bound, node.val])
+                q.append([node.right, node.val, right_bound])
+
+        return True
