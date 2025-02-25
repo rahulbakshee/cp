5dc770d102c49bdc4479523ce09b545c065ffdf1
Rahul Bakshee <rhlbks@gmail.com>
2025-02-25 14:47:04 -0800

    Update lc0036-Valid Sudoku.py

--- LeetCode/lc0036-Valid Sudoku.py
+++ LeetCode/lc0036-Valid Sudoku.py
@@ -1,5 +1,35 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        blocks = defaultdict(set)
+
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if board[r][c] != ".":
+                    if (board[r][c] in rows[r] or
+                        board[r][c] in cols[c] or
+                        board[r][c] in blocks[r//3, c//3]):
+                        return False
+                    
+                    # if current number is not there in any of the sets
+                    # then update those sets with this number
+                    rows[r].add(board[r][c])
+                    cols[c].add(board[r][c])
+                    blocks[r//3, c//3].add(board[r][c])
+        return True
+                    
+
+
+
+
+
+
+
+
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
         # create three separate sets for storing numbers of sudoku cells
         rows = collections.defaultdict(set)
         cols = collections.defaultdict(set)
