465c2e00bb87622c3d16214ffcf993d186e56843
Rahul Bakshee <rhlbks@gmail.com>
2025-02-26 13:21:30 -0800

    Update lc0735-Asteroid Collision.py

--- LeetCode/lc0735-Asteroid Collision.py
+++ LeetCode/lc0735-Asteroid Collision.py
@@ -1,3 +1,48 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        # stack = []
+        # prev = stack[-1]
+        
+        # Overall cases
+        # 1- both moving same direction(positive, positive) - No collision
+        # 2- both moving same direction(negative, negative) - NO collision
+        # 3- both moving in opposite direction
+            # 3.1- prev is positive, upcoming is negative - YES collision
+            # 3.2- prev is negative, upcoming is positive - NO collision
+
+        # stack = [1,2,3] , asteroid = -4
+        # result = [-4]
+
+        stack = []
+        for ast in asteroids:
+            if not stack:
+                stack.append(ast)
+                continue
+
+            # there is something ins stack
+            # compare on conditions
+            # 3.1
+            while stack and stack[-1] > 0 and ast < 0:
+                if stack[-1] > -1*ast:
+                    break
+                elif stack[-1] < -1*ast:
+                    stack.pop()
+                    continue
+                else:
+                    stack.pop()
+                    break
+            # 1,2,3.2
+            else:
+                stack.append(ast)
+
+
+        return stack
+
+
+
+
+
+
 # time:O(n), space:O(n)
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
