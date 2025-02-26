ab78ade02e8cdb3402ec4c4d53bc6eeb33732f78
Rahul Bakshee <rhlbks@gmail.com>
2025-02-25 21:17:44 -0800

    Update lc0771-Jewels and Stones.py

--- LeetCode/lc0771-Jewels and Stones.py
+++ LeetCode/lc0771-Jewels and Stones.py
@@ -1,18 +1,12 @@
-# https://leetcode.com/problems/jewels-and-stones/
-
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        stones = [s for s in stones]
-        d = {}
-        counter = 0
-        for s in stones:
-            if s in d:
-                d[s] += 1
-            else:
-                d[s] = 1
+        set_jewels = set(jewels)
+        hashmap_stones = Counter(stones)
         
-        for j in jewels:
-            if j in d.keys():
-                counter += d[j]
+        
+        count = 0
+        for key, value in hashmap_stones.items():
+            if key in set_jewels:
+                count += value
                 
-        return counter
+        return count
