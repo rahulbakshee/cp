911d0e52fffb68c4f5cd1aa753354fb2abcfc861
Rahul Bakshee <rhlbks@gmail.com>
2025-02-26 16:51:51 -0800

    Update lc1905-Count Sub Islands.py

--- LeetCode/lc1905-Count Sub Islands.py
+++ LeetCode/lc1905-Count Sub Islands.py
@@ -1,30 +1,85 @@
-# time:O(n*m), space:O(n*m)
+# DFS - recursion
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
-        rows, cols = len(grid1), len(grid1[0])
-        visited = set()
-
-        def dfs(r, c):
+        def dfs(row, col):
             # base case
-            if r<0 or c<0 or r>=rows or c>= cols or grid2[r][c] == 0 or (r,c) in visited:
+            if (row<0 or row>=rows or
+                col<0 or col>=cols or
+                (row,col) in visited or
+                grid2[row][col] == 0):
                 return True
 
-            if grid1[r][c] == 0:
-                return False
 
-            visited.add((r,c))
-            # recurse
-            return dfs(r-1,c) & dfs(r+1,c) & dfs(r,c-1) & dfs(r,c+1) # usage of & instead of "and"
-            # In python, x and y means if x if False, return x. otherwise, return y. 
-            # So only if x is True then y can be executed. 
-            # x or y: if x if True, return x. otherwise return y. That means if x is True, y will not be executed.
-            # So in the dfs(). we can only use &. Cause it is the bit counting. 
+            # update visited set
+            visited.add((row,col))
 
-    
-        counter = 0
+            # check in grid1 if it is land or water
+            if grid1[row][col] == 0:
+                result = False
+            else:
+                result = True
+
+            # explore neighbors
+            for dr, dc in directions:
+                new_row = row+dr
+                new_col = col+dc
+                result = dfs(new_row, new_col) and result
+
+            return result
+
+
+        directions = [(0,1), (1,0), (-1, 0), (0,-1)]
+        rows = len(grid1)
+        cols = len(grid1[0])
+        visited = set()
+        count = 0
+
         for r in range(rows):
             for c in range(cols):
-                if grid2[r][c] == 1 and (r, c) not in visited and dfs(r,c):
-                    counter += 1
+                if grid2[r][c] == 1 and (r,c) not in visited:
+                    count += dfs(r,c)    
 
-        return counter
+        return count
+
+
+# DFS - stack
+class Solution:
+    def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
+        
+        def dfs(r,c):
+            stack = [(r, c)]
+            visited.add((r,c))
+            result = True
+
+            while stack:
+                row, col = stack.pop()
+
+                if grid1[row][col] == 0:
+                    result = False
+
+                for dr, dc in directions:
+                    new_row = row+dr
+                    new_col = col+dc
+                    if not (new_row<0 or new_row>=rows or
+                            new_col<0 or new_col>=cols or
+                            (new_row,new_col) in visited or
+                            grid2[new_row][new_col] == 0):
+                        stack.append((new_row, new_col))
+                        visited.add((new_row, new_col))
+
+            return result
+
+
+        directions = [(0,1), (1,0), (-1, 0), (0,-1)]
+        rows = len(grid1)
+        cols = len(grid1[0])
+        visited = set()
+        count = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid2[r][c] == 1 and (r,c) not in visited:
+                    if dfs(r,c):
+                        count += 1
+
+        return count
