9b57638593d75a2651a07f809161d34aaea07407
Rahul Bakshee <rhlbks@gmail.com>
2025-02-27 19:23:15 -0800

    Update lc0023-Merge k Sorted Lists.py

--- LeetCode/lc0023-Merge k Sorted Lists.py
+++ LeetCode/lc0023-Merge k Sorted Lists.py
@@ -22,3 +22,55 @@ class Solution:
             curr = curr.next
 
         return dummy.next
+
+
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        total_len = len(lists)
+
+        interval = 1
+        while interval < total_len:
+            for i in range(0, total_len-interval, interval*2):
+                lists[i] = self.merge_pairs(lists[i], lists[i+interval])
+            interval *= 2
+
+        return lists[0] if total_len >0 else None
+
+
+    def merge_pairs(self, list1, list2):
+        if not list1 and not list2:
+            return 
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+
+        # while both are no empty
+        # compare them 
+        dummy = head = ListNode(-1)
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                head.next = list1
+                list1 = list1.next
+            else:
+                head.next = list2
+                list2 = list2.next
+
+            head = head.next
+
+        # if either of them are non empty
+        # append them to the final result
+        if list1:
+            head.next = list1
+        if list2:
+            head.next = list2
+
+        return dummy.next
