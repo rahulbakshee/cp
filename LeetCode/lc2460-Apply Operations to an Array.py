6d6701854a3710d81b2b91b5b0ae5e4a891984f3
Rahul Bakshee <rhlbks@gmail.com>
2025-02-28 21:05:54 -0800

    Update lc2460-Apply Operations to an Array.py

--- LeetCode/lc2460-Apply Operations to an Array.py
+++ LeetCode/lc2460-Apply Operations to an Array.py
@@ -13,3 +13,29 @@ class Solution:
                 nums[index], nums[i] = nums[i], nums[index]
                 index += 1
         return nums
+
+
+
+# solved myself in March 2025 monthly challange
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        left, right = 0, 1
+        while right < len(nums):
+            if nums[left] == nums[right]:
+                nums[left] = nums[left]*2
+                nums[right] = 0
+                left += 2
+                right += 2
+            else:
+                left += 1
+                right += 1
+
+
+        result = [0] * len(nums)
+        i = 0
+        for num in nums:
+            if num != 0:
+                result[i] = num
+                i += 1
+
+        return result
