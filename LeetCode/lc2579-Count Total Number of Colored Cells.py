58b32cdf2e56e078173ee0856d9ac1f8b0167ebf
Rahul Bakshee <rhlbks@gmail.com>
2025-03-04 21:51:43 -0800

    Update lc2579-Count Total Number of Colored Cells.py

--- LeetCode/lc2579-Count Total Number of Colored Cells.py
+++ LeetCode/lc2579-Count Total Number of Colored Cells.py
@@ -1,3 +1,19 @@
+class Solution:
+    def coloredCells(self, n: int) -> int:
+        result = 1
+        while n:
+            result = result + 4*(n-1)
+            n -= 1
+
+
+        return result
+
+
+class Solution:
+    def coloredCells(self, n: int) -> int:
+        return 1 + n*(n-1)*2
+
+
 # time:O(1), space:O(1)
 class Solution:
     def coloredCells(self, n: int) -> int:
