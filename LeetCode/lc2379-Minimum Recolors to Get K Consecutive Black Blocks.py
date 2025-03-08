28cb431a68b77d884bdd370d0ed4df3e76472b43
Rahul Bakshee <rhlbks@gmail.com>
2025-03-07 16:42:44 -0800

    Update lc2379-Minimum Recolors to Get K Consecutive Black Blocks.py

--- LeetCode/lc2379-Minimum Recolors to Get K Consecutive Black Blocks.py
+++ LeetCode/lc2379-Minimum Recolors to Get K Consecutive Black Blocks.py
@@ -1,3 +1,31 @@
+# two pointers - time:O(n), space:O(1)
+class Solution:
+    def minimumRecolors(self, blocks: str, k: int) -> int:
+        whites = 0
+        min_whites = len(blocks)
+
+        left, right = 0,0
+        while right < len(blocks):
+            if blocks[right] == "W":
+                whites += 1
+            
+            # reduce the window
+            if right-left+1 > k:
+                if blocks[left] == "W":
+                    whites -= 1
+                left += 1
+            
+            # calculate the min_whites if window length is k
+            if right-left+1 == k:
+                min_whites = min(min_whites, whites)
+            
+            
+            right += 1
+
+        return min_whites
+
+
+
 # time:O(blocks), space:O(k)
 from collections import Counter
 class Solution:
