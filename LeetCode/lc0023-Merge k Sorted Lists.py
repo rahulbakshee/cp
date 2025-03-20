2c30fa47824c6b85584f71eb2e3afd08c5397a4b
Rahul Bakshee <rhlbks@gmail.com>
2025-03-20 15:20:12 -0700

    Update lc0023-Merge k Sorted Lists.py

--- LeetCode/lc0023-Merge k Sorted Lists.py
+++ LeetCode/lc0023-Merge k Sorted Lists.py
@@ -1,11 +1,10 @@
+# 1- bruteforce - sorting  - time:O(nlogn), space:O(n)
+# get all values from all the linked lists
+# store them in an array/list
+# sort them in ASC
+# create a new llinkedlist and put the values of listnodes from newly created array
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        # 1- bruteforce - sorting  - time:O(nlogn), space:O(n)
-        # get all values from all the linked lists
-        # store them in an array/list
-        # sort them in ASC
-        # create a new llinkedlist and put the values of listnodes from newly created array
-
         all_nodes = []
         for l in lists:
             while l:
@@ -24,13 +23,64 @@ class Solution:
         return dummy.next
 
 
+# 2 - Convert merge k lists problem to merge 2 lists (k-1) times. 
+# time:O(kn), space:O(1) - k is the numbe of linkedlists, n total number of nodes in two LL
+"""
+l1, l2, l3, l4, l5
+\    /
+  l12
+   \    /
+    l123
+      \    /
+       l1234
+          \     /
+            l12345
+"""
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        def mergeTwoLists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+            def recurse(list1, list2):
+                # base cases
+                if not list1 and not list2:
+                    return  
+                if not list1:
+                    return list2
+                if not list2:
+                    return list1
+
+                # compare element by element from the two lists
+                if list1.val < list2.val:
+                    list1.next = recurse(list1.next, list2)
+                    return list1
+                else:
+                    list2.next = recurse(list1, list2.next)
+                    return list2
+
+            return recurse(list1, list2)  
+
+        result = None
+        for i in range(len(lists)):
+            result = mergeTwoLists(result, lists[i])
+
+        return result
 
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+# 3- merge with divide and conquer
+# Pair up k lists and merge each pair.
+# After the first pairing, k lists are merged into k/2 lists with average 2N/k length, then k/4, k/8 and so on.
+# Repeat this procedure until we get the final sorted linked list.
+# Thus, we'll traverse almost N nodes per pairing and merging, and repeat this procedure about log k base 2 times.
+# time:O(nlogk), space:O(1)
+"""
+l1, l2, l3, l4, l5, l6
+\    /   \   /   \  /
+  l12     l34     l56
+   \      /        /
+    l1234        /
+      \       /
+        l123456
+
+"""
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         total_len = len(lists)
@@ -74,3 +124,20 @@ class Solution:
             head.next = list2
 
         return dummy.next
+
+
+
+########################################
+# important thing to rememebr - 
+
+total_len = len(lists)
+if total_len == 0:
+    return 
+
+interval = 1
+while interval < total_len:
+    for i in range(0, total_len - interval, interval *2):
+        lists[i] = mergeTwoLists(lists[i], lists[i+interval])
+    interval *= 2
+
+return lists[0]
