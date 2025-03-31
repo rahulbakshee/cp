cc1939b52cf23da024314ae8931fa282bdb4eb65
Rahul Bakshee <rhlbks@gmail.com>
2025-03-31 13:39:09 -0700

    Update lc0743-Network Delay Time.py

--- LeetCode/lc0743-Network Delay Time.py
+++ LeetCode/lc0743-Network Delay Time.py
@@ -30,3 +30,29 @@ class Solution:
         if value != float("inf"):
             return value
         return -1
+
+
+
+
+# DFS
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # 1 - build graph
+        graph = defaultdict(list)
+        for u,v,w in times:
+            graph[u].append((v,w))
+
+        # 2 - init dsa for storage
+        times = {node:float("inf") for node in range(1, n+1)}      
+
+        # 3 - def dfs
+        def dfs(curr_node, curr_time):
+            if curr_time < times[curr_node]:
+                times[curr_node] = curr_time
+
+                for nei, t in graph[curr_node]:
+                    dfs(nei, curr_time + t)
+
+        dfs(k,0)
+        result = max(times.values())
+        return -1 if result == float("inf") else result
