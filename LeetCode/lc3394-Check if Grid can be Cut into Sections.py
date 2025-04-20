8e6b15b94c768a6bf3af6f2896578721b1be3142
Rahul Bakshee <rhlbks@gmail.com>
2025-04-20 15:45:00 -0700

    Update lc3394-Check if Grid can be Cut into Sections.py

--- LeetCode/lc3394-Check if Grid can be Cut into Sections.py
+++ LeetCode/lc3394-Check if Grid can be Cut into Sections.py
@@ -1,3 +1,39 @@
+# better way - neetcode as well
+
+class Solution:
+    def checkValidCuts(self, n: int, rectangles: List[List[int]]) -> bool:
+        
+        # 1 - separate x and y direction intervals
+        # and sort them
+        x = [[rect[0],rect[2]] for rect in rectangles]
+        y = [[rect[1],rect[3]] for rect in rectangles]
+
+        x.sort()
+        y.sort()
+
+        #2 - create a function to count non overlapping intervals
+        def count_non_overlapping(intervals):
+            count = 0
+            prev_end = -1
+
+            for start, end in intervals:
+                if prev_end <= start:
+                    count += 1
+                prev_end = max(prev_end, end)
+
+            return count
+
+        # 3 - use this function on bothg directions
+        return max(count_non_overlapping(x), count_non_overlapping(y)) >= 3
+
+        
+
+
+
+
+# IGNORE THIS ONE  -= THIS IS FROM EDITORIAL
+# USE THE ABOVE ONE 
+
 class Solution:
     def checkValidCuts(self, n: int, rectangles: List[List[int]]) -> bool:
         def check(rectangles, dim):
@@ -18,3 +54,6 @@ class Solution:
             return count >=2
 
         return check(rectangles, 0) or check(rectangles, 1)
+
+
+
