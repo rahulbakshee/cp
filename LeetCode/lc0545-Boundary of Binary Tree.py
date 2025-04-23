b34dd9d77bf955745d9092f36e058e3e2ab092d1
Rahul Bakshee <rhlbks@gmail.com>
2025-04-23 11:34:45 -0700

    Update lc0545-Boundary of Binary Tree.py

--- LeetCode/lc0545-Boundary of Binary Tree.py
+++ LeetCode/lc0545-Boundary of Binary Tree.py
@@ -4,6 +4,80 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+class Solution:
+    def boundaryOfBinaryTree(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+
+        # 1-collect left_boundary
+        def left_boundary(node):
+            if not node:
+                return 
+            # if leaf node
+            if not node.left and not node.right:
+                return
+
+            left.append(node.val)
+
+            # if not leaf, start traversal from left child
+            if node.left:
+                left_boundary(node.left)
+            else:
+                left_boundary(node.right)          
+
+        # 2-collect leaf nodes
+        def leaf_nodes(node):
+            if not node.left and not node.right:
+                leaves.append(node.val)
+            if node.left:
+                leaf_nodes(node.left)
+            if node.right:
+                leaf_nodes(node.right)
+
+        # 3-collect right_boundary
+        def right_boundary(node):
+            if not node:
+                return 
+            # if leaf node
+            if not node.left and not node.right:
+                return
+            
+            # if not leaf, then start traversal from right child then left child
+            if node.right:
+                right_boundary(node.right)
+            else:
+                right_boundary(node.left)
+
+            right.append(node.val)
+
+        
+        left, leaves, right = [], [], []
+        if not root:
+            return []
+        if not root.left and not root.right:
+            return [root.val]
+
+        left_boundary(root.left)
+        leaf_nodes(root)
+        right_boundary(root.right)
+
+        return [root.val] + left + leaves + right
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # time:O(n), space:O(n)
 class Solution:
     def boundaryOfBinaryTree(self, root: Optional[TreeNode]) -> List[int]:
