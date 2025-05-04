193c7e0039b6e3511c54960198c782afca1268c0
Rahul Bakshee <rhlbks@gmail.com>
2025-05-03 18:05:08 -0700

    Update lc0140-Word Break II.py

--- LeetCode/lc0140-Word Break II.py
+++ LeetCode/lc0140-Word Break II.py
@@ -50,4 +50,23 @@ class Solution:
         dp(0,())
         return result
 
+# exponential - I DID IT MYSELF
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        def dp(i, sol):
+            if i >= len(s):
+                result.append(" ".join(sol))
+                return
+
+
+            for word in wordDict:
+                w = len(word)
+                if i+w<=len(s) and s[i:i+w] == word:
+                    dp(i+w, sol + [word])
+                    
+            
+        result = []
+        dp(0, [])
+        return result     
+
 
