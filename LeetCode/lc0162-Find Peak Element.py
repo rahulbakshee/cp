0983f85266752164a189e85fea1f748bb6f11750
Rahul Bakshee <rhlbks@gmail.com>
2025-05-05 00:06:26 -0700

    Update lc0162-Find Peak Element.py

--- LeetCode/lc0162-Find Peak Element.py
+++ LeetCode/lc0162-Find Peak Element.py
@@ -1,3 +1,13 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        nums = [float("-inf")] + nums + [float("-inf")]
+
+        for i in range(1, len(nums)-1):
+            if nums[i-1] < nums[i] > nums[i+1]:
+                return i-1
+
+        
+
 # time:O(logn), space:O(1)
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
