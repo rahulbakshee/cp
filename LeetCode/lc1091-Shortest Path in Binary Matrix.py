8448dca90d01a03a9279f5cc9d1c22f3ef7b6b83
Rahul Bakshee <rhlbks@gmail.com>
2025-05-07 12:22:46 -0700

    Update lc1091-Shortest Path in Binary Matrix.py

--- LeetCode/lc1091-Shortest Path in Binary Matrix.py
+++ LeetCode/lc1091-Shortest Path in Binary Matrix.py
@@ -2,41 +2,44 @@
 from collections import deque
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        if not grid or grid[0][0] == 1:
+        rows = len(grid)
+        cols = len(grid[0])
+        
+        if not grid or rows == 0:
+            return -1
+        
+        if grid[0][0] == 1 or grid[rows-1][cols-1] == 1:
             return -1
 
-        
-        n = len(grid)
+        directions = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
         visited = set()
-        directions = [(0,1),(1,0),(0,-1),(-1,0),(1,1),(-1,1),(1,-1),(-1,-1)]
+
+        q = deque([(0,0,1)])
+        while q:
+            for _ in range(len(q)):
+                r,c,length = q.popleft()
+
+                if (r,c) == (rows-1,cols-1):
+                    return length
+
+                # explore children
+                for dr,dc in directions:
+                    new_r = r+dr
+                    new_c = c+dc
+                    
+                    if new_r<0 or new_r>=rows or new_c<0 or new_c>=cols:
+                        continue
+
+                    if grid[new_r][new_c]:
+                        continue
+                    
+                    if (new_r,new_c) in visited:
+                        continue
+                    
+                    visited.add((new_r,new_c))
+                    q.append([new_r,new_c,length+1])
+
         
-        queue = deque([(0,0,1)]) # row, col, length
-        visited.add((0,0))
-
-        while queue:
-            row,col,distance = queue.popleft()
-            # check if this is the target position
-            if row == n-1 and col == n-1:
-                return distance
-
-            # explore its neighbors/children
-            for dr,dc in directions:
-                new_row = row+dr
-                new_col = col+dc
-
-                # check for index out of bounds
-                if new_row<0 or new_row>=n or new_col<0 or new_col>=n:
-                    continue
-
-                # check if value = 1, then continue
-                if grid[new_row][new_col]:
-                    continue
-
-                # check if not in visited:
-                if (new_row, new_col) not in visited:
-                    queue.append((new_row, new_col, distance+1))
-                    visited.add((new_row, new_col))
-
         return -1
 
 
