7d6fe7903356907c19da120982b13c45f2c4807e
Rahul Bakshee <rhlbks@gmail.com>
2025-05-08 13:33:34 -0700

    Update lc0359-Logger Rate Limiter.py

--- LeetCode/lc0359-Logger Rate Limiter.py
+++ LeetCode/lc0359-Logger Rate Limiter.py
@@ -1,3 +1,5 @@
+# using hashmap
+# time:O(1), space:O(n)
 class Logger:
 
     def __init__(self):
@@ -13,6 +15,39 @@ class Logger:
                 return True
             else:
                 return False
+
+
+
+# using QUEUE + SET
+# time:O(n), space:O(n)
+from collections import deque
+
+class Logger:
+
+    def __init__(self):
+        self.hashset = set()
+        self.q = deque()
+        self.diff = 10
+
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+        # clean up expired messages using sliding window of timestamps
+        while self.q:
+            msg, ts = self.q[0]
+            if ts + self.diff <= timestamp:
+                self.q.popleft()
+                self.hashset.remove(msg)
+            else:
+                break
+
+        # check for upcoming msg and timestamp
+        if message not in self.hashset:
+            self.hashset.add(message)
+            self.q.append((message, timestamp))
+            return True
+
+        else:
+            return False
+
 
 
 
