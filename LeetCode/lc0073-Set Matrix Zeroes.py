e0c4bb568363a9c737d93cfda34e4864171abbff
Rahul Bakshee <rhlbks@gmail.com>
2025-05-21 12:14:07 -0700

    Update lc0073-Set Matrix Zeroes.py

--- LeetCode/lc0073-Set Matrix Zeroes.py
+++ LeetCode/lc0073-Set Matrix Zeroes.py
@@ -24,3 +24,47 @@ class Solution:
                     matrix[i][j] = 0
 
         
+
+
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+
+        m = len(matrix)
+        n = len(matrix[0])
+
+        frz = False
+        fcz = False
+
+        for i in range(m):
+            if matrix[i][0] == 0:
+                fcz = True
+                break
+        
+        for i in range(n):
+            if matrix[0][i] == 0:
+                frz = True
+                break
+
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:        
+                    matrix[i][j] = 0
+
+        if frz:
+            for i in range(n):
+                matrix[0][i] = 0
+
+                            
+        if fcz:
+            for i in range(m):
+                matrix[i][0] = 0
