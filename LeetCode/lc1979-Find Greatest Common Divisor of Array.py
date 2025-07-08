b37e10be1042220c259b7823b37581c3fb9bc716
Rahul Bakshee <rhlbks@gmail.com>
2025-07-07 21:34:24 -0700

    Update lc1979-Find Greatest Common Divisor of Array.py

--- LeetCode/lc1979-Find Greatest Common Divisor of Array.py
+++ LeetCode/lc1979-Find Greatest Common Divisor of Array.py
@@ -1,42 +1,28 @@
-# sorting - time:O(nlogn), space:O(sorting/n)
+# recursive way
+# time:O(max(a, b)), space:O(max(a, b))
 class Solution:
     def findGCD(self, nums: List[int]) -> int:
-        nums.sort()
-        a = nums[0]
-        b = nums[-1]
+        min_num = min(nums)
+        max_num = max(nums)
 
-        # # iterative
-        # def gcd(a,b):
-        #     while b > 0:
-        #         a,b = b, a%b
-        #     return a
-
-        # recursive
-        def gcd(a, b):
-            if b == 0:
-                return a
-            return gcd(b, a%b)
-        return gcd(a,b)
-
-# single pass - time:O(n), space:O(1)
-class Solution:
-    def findGCD(self, nums: List[int]) -> int:
-
-        min_num  = max_num = nums[0]
-        for num in nums:
-            min_num  = min(num, min_num)
-            max_num = max(num, max_num)
-
-        # # iterative
-        # def gcd(a,b):
-        #     while b:
-        #         a,b = b, a%b
-        #     return a
-        
-        # recursive
         def gcd(a,b):
             if b == 0:
                 return a
             return gcd(b, a%b)
+
         return gcd(min_num, max_num)
-      
+
+
+# iterative way
+# time:O(max(a, b)), space:O(1)
+class Solution:
+    def findGCD(self, nums: List[int]) -> int:
+        min_num = min(nums)
+        max_num = max(nums)
+
+        def gcd(a,b):
+            while b:
+                a, b = b, a%b
+            return a
+
+        return gcd(min_num, max_num)
