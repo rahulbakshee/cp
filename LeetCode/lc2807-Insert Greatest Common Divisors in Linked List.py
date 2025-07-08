eae5e47e57247611194e7a3d95693b21e10b7a85
Rahul Bakshee <rhlbks@gmail.com>
2025-07-07 22:07:06 -0700

    Update lc2807-Insert Greatest Common Divisors in Linked List.py

--- LeetCode/lc2807-Insert Greatest Common Divisors in Linked List.py
+++ LeetCode/lc2807-Insert Greatest Common Divisors in Linked List.py
@@ -1,59 +1,71 @@
-# euclid algo for GCD - time:O(n*logm), space:O(1)
+"""
+Algorithm
+Main method insertGreatestCommonDivisors:
+
+If the list contains only one node (head.next is null), return the head 
+as no insertion is needed.
+Initialize ListNode variables node1 and node2 to head and head.next 
+respectively, to traverse the linked list.
+While node2 is not null:
+    Calculate the GCD's of the values in node1 and node2.
+    Create a new ListNode gcdNode with the calculated GCD value.
+    Update node1.next to gcdNode.
+    Update gcdNode.next to node2.
+    Set node1 to node2 and node2 to node2.next, respectively. This essentially 
+    moves node1 and node2 to the next pair of nodes in the list.
+Return the modified head of the list as our answer.
+Complexity Analysis
+Let n be the number of nodes in the linked list.
+
+Time complexity: O(n⋅log(min(a,b)))
+
+The algorithm traverses the list, visiting each node exactly once. 
+This takes linear time. The GCD is calculated using the Euclidean algorithm, 
+which has a time complexity of O(log(min(a,b))), where a and b are 
+numbers whose GCD is being calculated.
+
+Thus, the overall time complexity of the algorithm is O(n⋅log(min(a,b))).
+
+Space complexity: O(1)
+
+The iterative implementation of the GCD method has a space complexity of O(1).
+
+"""
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        # recursive way
         def gcd(a,b):
-            while b!= 0:
+            if b == 0:
+                return a
+            return gcd(b, a%b)
+
+        # iterative way
+        def gcd(a,b):
+            while b:
                 a, b = b, a%b
             return a
 
-        # # recursive 
-        # def gcd(a,b):
-        #     if b ==0:
-        #         return a
-        #     return gcd(b,a%b)
+        # iterate over linkedlist two nodes at at time
+        # and find gcd. insert a node with that value
 
         if not head or not head.next:
             return head
 
         curr = head
         while curr and curr.next:
-            gcd_val = gcd(curr.val, curr.next.val)
-            temp = ListNode(gcd_val)
+            val = gcd(curr.val, curr.next.val)
+            new_node = ListNode(val)
 
             # assign pointers
-            temp.next = curr.next
-            curr.next = temp
-
-            # move curr to next
-            curr = curr.next.next
-        return head
-
-
-
-# LONGER - time:O(n*m) - nis len of LL, m is max val of any node in all the nodes
-class Solution:
-    def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next:
-            return head
-
-        def gca(a, b):
-            max_gca = 1
-            for i in range(1, min(a,b)+1):
-                if a%i==0 and b%i==0:
-                    max_gca = max(max_gca, i)
-            return max_gca
-
-        curr = head
-        curr.next = head.next
-        while curr and curr.next:
-            temp_val = gca(curr.val, curr.next.val)
-            temp = ListNode(temp_val)
-
-            # pointers
-            temp.next = curr.next
-            curr.next = temp
-            
-            # increase the curr 
-            curr = curr.next.next
+            new_node.next = curr.next
+            curr.next = new_node
+                        
+            curr = curr.next.next        
 
         return head
