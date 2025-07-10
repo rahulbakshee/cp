066ff191cf185312db4b6034da228729bae2559d
Rahul Bakshee <rhlbks@gmail.com>
2025-07-10 13:15:07 -0700

    Update lc0134-Gas Station.py

--- LeetCode/lc0134-Gas Station.py
+++ LeetCode/lc0134-Gas Station.py
@@ -1,16 +1,44 @@
-# time:O(n), space:O(1)
+# bruteforce - time:O(n^2), space:O(1)
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+
+        for start in range(len(gas)):
+            total = 0
+            completed = True
+        
+
+            for i in range(len(gas)):
+                index = (start+i) % len(gas)
+
+                total += gas[index] - cost[index]
+
+                if total < 0:
+                    completed = False
+                    break
+
+            if completed:
+                return start
+
+        return -1        
+
+
+
+# greedy - time:O(n), space:O(1)
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         if sum(gas) < sum(cost):
             return -1
 
         total = 0
-        index = 0
+        start = 0
+
         for i in range(len(gas)):
             total += gas[i] - cost[i]
 
             if total < 0:
                 total = 0
-                index = i+1
+                start = i+1
 
-        return index
+        return start
