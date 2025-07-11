bf2f82f812e0f3e9dc0b8e13654f2ee4f4552c41
Rahul Bakshee <rhlbks@gmail.com>
2025-07-11 12:32:48 -0700

    Update lc0997-Find the Town Judge.py

--- LeetCode/lc0997-Find the Town Judge.py
+++ LeetCode/lc0997-Find the Town Judge.py
@@ -1,31 +1,20 @@
-# two dicts
-# time:O(T+n), space:O(n)
+"""track how many people trust each person (incoming) and how many 
+people each person trusts (outgoing). The judge must have n-1 
+incoming trusts and 0 outgoing trusts."""
+# time:O(E+V), space:O(V)
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        in_trust_dict = defaultdict(int)
-        out_trust_dict = defaultdict(int)
+        # build two graphs of trusts
+        incoming  = defaultdict(int)
+        outgoing = defaultdict(int)
 
-        for t in trust:
-            out_trust_dict[t[0]] += 1
-            in_trust_dict[t[1]] += 1
+        for u, v in trust:
+            outgoing[u] += 1
+            incoming[v] += 1
 
+        # check for n-1 incoming and 0 outgoing
         for i in range(1, n+1):
-            if in_trust_dict[i] == n-1 and out_trust_dict[i] == 0:
+            if outgoing[i] == 0 and incoming[i] == n-1:
                 return i
 
-        return -1        
-
-# single dict
-# time:O(T+n), space:O(n)
-class Solution:
-    def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        trust_dict = defaultdict(int)
-        for t in trust:
-            trust_dict[t[0]] -= 1
-            trust_dict[t[1]] += 1
-
-        for i in range(1, n+1):
-            if trust_dict[i] == n-1:
-                return i
         return -1
-        
