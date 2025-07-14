56ac3a1ac24d6c446147e9e4b27e1182413dc09f
Rahul Bakshee <rhlbks@gmail.com>
2025-07-14 12:27:47 -0700

    Update lc0695-Max Area of Island.py

--- LeetCode/lc0695-Max Area of Island.py
+++ LeetCode/lc0695-Max Area of Island.py
@@ -1,98 +1,230 @@
-# DFS - time:O(m*n), space:O(m*n) + recusrive callstack
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        rows = len(grid)
+        # check for dimensions of the input grid
+        # 
+
+        # approaches to traverse and calculates the max area - 
+        # m - rows, n - cols
+        # DFS - recursion - time:O(mn), space:O(mn)
+        # DFS - iterative - using stack - time:O(mn), space:O(mn)
+        # bfs - using queue - time:O(mn), space:O(mn)
+        # union find  - using disjoint set - time:O(mn), space:O(mn)
+
+        # traverse from a land area and explore its neighbors
+        # if neighbors are water ->ignore, if land ->traverse
+
+        # keep a counter of area 
+        # return the max_area
+
+
+        # DFS - recursive approach
+
+        def dfs(r,c):
+            # base case - out of boundry
+            if r<0 or r>=row or c<0 or c>=cols:
+                return 0
+
+            # base case - water
+            if grid[r][c] == 0:
+                return 0
+
+            # base case - if already visited
+            if (r,c) in visited:
+                return 0
+            
+            # mark as visited
+            visited.add((r,c))
+
+            curr_area = 1
+            # explore its neighbors
+            for dr, dc in directions:
+                new_r = r+dr
+                new_c = c+dc
+                curr_area += dfs(new_r, new_c)
+            return curr_area
+
+        row = len(grid)
         cols = len(grid[0])
+
+        # if not grid or rows == 0 or cols == 0:
+        # return 0
+        directions = [(0,1),(1,0),(0,-1),(-1,0)]
         visited = set()
         max_area = 0
 
-
-        def dfs(r,c):
-            if r<0 or c<0 or r>= rows or c>=cols or (r,c) in visited or grid[r][c] == 0:
-                return 0
-
-            visited.add((r,c))
-            return 1+ dfs(r+1,c) + dfs(r,c+1) + dfs(r-1,c) + dfs(r,c-1)
-
-
-        for i in range(rows):
+        for i in range(row):
             for j in range(cols):
-                if grid[i][j] == 1 and (i,j) not in visited:
-                    curr_area = dfs(i, j)
-                    max_area = max(max_area, curr_area)
+                if (i,j) not in visited and grid[i][j]:
+                    area = dfs(i,j)
+                    max_area = max(max_area, area)
 
         return max_area
 
 
 
-# bfs - queue - time:O(m*n), space:O(m*n)
+
+# DFS iterative-using stack
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-        visited = set()
-        max_area = 0
-
-        def bfs(r,c):
-            q = collections.deque()
-            q.append((r,c))
-            visited.add((r,c))
-            area = 0
-
-            while q:
-                row,col = q.popleft()
-                area += 1
-                for x,y in [(row+1,col), (row-1,col), (row,col+1), (row,col-1)]:
-                    if 0<=x<rows and 0<=y<cols and (x,y) not in visited and grid[x][y]==1:
-                        q.append((x,y))
-                        visited.add((x,y))
-            return area
-
-        
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == 1 and (i,j) not in visited:
-                    curr_area = bfs(i,j)
-                    max_area = max(max_area, curr_area)
-
-
-
-
-
-# dfs - stack
-class Solution:
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-        visited = set()
-        max_area = 0
-
-
-        def dfs(r,c):
-            stack = []
-            stack.append((r,c))
-            visited.add((r,c))
+        def dfs(i,j):
+            """stack based DFS implementation"""
+            stack = [[i,j]]
             area = 0
 
             while stack:
-                row, col = stack.pop()
-                area += 1
+                r,c = stack.pop()
+                # check for in boundry
+                if r<0 or r>=rows or c<0 or c>=cols:
+                    continue
+                # check for water/land
+                if grid[r][c] == 0:
+                    continue
+                # check if already visited
+                if (r,c) in visited:
+                    continue
 
-                for x,y in [(row+1,col), (row-1,col), (row,col+1), (row,col-1)]:
-                    if 0<=x<rows and 0<=y<cols and (x,y) not in visited and grid[x][y]:
-                        stack.append((x,y))
-                        visited.add((x,y))
+                visited.add((r,c))
+                # account for area
+                area += 1
+                
+                # explore the neighbors
+                for dr, dc in directions:
+                    new_r = r+dr
+                    new_c = c+dc
+                    stack.append((new_r,new_c))       
 
             return area
 
+        
+        
+        rows = len(grid)
+        cols = len(grid[0])
+        visited = set()
+        directions = [(0,1),(1,0),(0,-1),(-1,0)]
+        max_area = 0
 
         for i in range(rows):
             for j in range(cols):
-                if (i,j) not in visited and grid[i][j] == 1:
-                    curr_area = dfs(i,j)
-                    max_area = max(max_area, curr_area)
+                if (i,j) not in visited and grid[i][j]:
+                    max_area = max(max_area, dfs(i,j))
         return max_area
 
 
+
+
+# BFS iterative-using queue
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        def bfs(i,j):
+            """queue based BFS implementation"""
+            q =  deque([[i,j]])
+            area = 0
+
+            while q:
+                r,c = q.popleft()
+                # check for in boundry
+                if r<0 or r>=rows or c<0 or c>=cols:
+                    continue
+                # check for water/land
+                if grid[r][c] == 0:
+                    continue
+                # check if already visited
+                if (r,c) in visited:
+                    continue
+
+                visited.add((r,c))
+                # account for area
+                area += 1
+                
+                # explore the neighbors
+                for dr, dc in directions:
+                    new_r = r+dr
+                    new_c = c+dc
+                    q.append((new_r,new_c))       
+
+            return area
+
+        
+        
+        rows = len(grid)
+        cols = len(grid[0])
+        visited = set()
+        directions = [(0,1),(1,0),(0,-1),(-1,0)]
+        max_area = 0
+
+        for i in range(rows):
+            for j in range(cols):
+                if (i,j) not in visited and grid[i][j]:
+                    max_area = max(max_area, bfs(i,j))
         return max_area
 
+
+
+
+
+# union find approach
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        # size or rank
+        self.size = [1] * n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        px = self.find(x)
+        py = self.find(y)
+
+        if px == py:
+            return
+
+        # union by size
+        if self.size[px] >= self.size[py]:
+            self.parent[py] = px
+            self.size[px] += self.size[py]
+        else:
+            self.parent[px] = py
+            self.size[py] += self.size[px]
+
+
+    def get_size(self, x):
+        px = self.find(x)
+        return self.size[px]
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+
+        directions = [(0,1),(1,0),(0,-1),(-1,0)]
+
+        uf = UnionFind(rows*cols)
+        max_area = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]:
+                    # explore neighbors
+                    for dr, dc in directions:
+                        new_r = r+dr
+                        new_c = c+dc
+
+                        # check inbounds
+                        if new_r<0 or new_r>=rows or new_c<0 or new_c>= cols:
+                            continue
+                        
+                        # check water
+                        if grid[new_r][new_c] == 0:
+                            continue
+
+                        # union them
+                        x1 = r*cols + c
+                        x2 = new_r*cols + new_c
+                        uf.union(x1, x2)
+                    
+                    max_area = max(max_area, uf.get_size(r*cols + c))                
+
+        return max_area
