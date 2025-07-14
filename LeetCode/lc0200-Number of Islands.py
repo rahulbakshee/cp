0d25b403cb50b3bae3b8190e6e437b01b9084fdf
Rahul Bakshee <rhlbks@gmail.com>
2025-07-14 10:03:30 -0700

    Update lc0200-Number of Islands.py

--- LeetCode/lc0200-Number of Islands.py
+++ LeetCode/lc0200-Number of Islands.py
@@ -146,3 +146,66 @@ class Solution:
                     islands += 1
                     
         return islands
+
+
+
+   
+# union find  - time:O(mn), space:O(mn)
+class DSU:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)] # initially parent of itself
+        self.rank = [1] * n                 # initially rank 1 for all
+
+    def find(self, x)->int:
+        # path compression
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        # find parent
+        px = self.find(x)
+        py = self.find(y)
+
+        if px == py:
+            return False
+
+        # union by rank
+        if self.rank[px] > self.rank[py]:
+            self.parent[py] = px
+            self.rank[px] += self.rank[py]
+        else:
+            self.parent[px] = py
+            self.rank[py] += self.rank[px]
+        return True
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        
+        dsu = DSU(rows*cols)
+
+        directions = [(0,1),(1,0),(0,-1),(-1,0)]
+        islands = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1":
+                    islands += 1
+                    # explore neighbors
+                    for dr, dc in directions:
+                        new_r = r+dr
+                        new_c = c+dc
+                        if (new_r<0 or new_r>=rows or 
+                            new_c<0 or new_c>=cols or  
+                            grid[new_r][new_c] == "0"):
+                            continue
+                        # find the index of the cell as we have a 2D grid
+                        # pass those indexes to union
+                        x = r*cols + c
+                        y = new_r*cols + new_c
+                        if dsu.union(x,y):
+                            islands -= 1
+
+        return islands
