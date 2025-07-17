93556eef7495992bffe7a42f1a1d0b1693d680ba
Rahul Bakshee <rhlbks@gmail.com>
2025-07-17 16:54:30 -0700

    Update lc0735-Asteroid Collision.py

--- LeetCode/lc0735-Asteroid Collision.py
+++ LeetCode/lc0735-Asteroid Collision.py
@@ -1,63 +1,33 @@
+"""(Stack Simulation)
+Use a stack to simulate asteroid movement and collision.
+Push asteroids moving right (> 0) onto the stack.
+When a left-moving asteroid (< 0) comes in, check for collisions with 
+the stack’s top (which will be a right-moving asteroid)."""
+# Overall cases
+# 1- both moving same direction(positive, positive) - No collision
+# 2- both moving same direction(negative, negative) - NO collision
+# 3- both moving in opposite direction
+    # 3.1- prev is positive, upcoming is negative - YES collision
+    # 3.2- prev is negative, upcoming is positive - NO collision"""
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        # stack = []
-        # prev = stack[-1]
-        
-        # Overall cases
-        # 1- both moving same direction(positive, positive) - No collision
-        # 2- both moving same direction(negative, negative) - NO collision
-        # 3- both moving in opposite direction
-            # 3.1- prev is positive, upcoming is negative - YES collision
-            # 3.2- prev is negative, upcoming is positive - NO collision
+        stack = []  # This stores the current surviving asteroids
 
-        # stack = [1,2,3] , asteroid = -4
-        # result = [-4]
-
-        stack = []
         for ast in asteroids:
-            if not stack:
-                stack.append(ast)
-                continue
-
-            # there is something ins stack
-            # compare on conditions
-            # 3.1
-            while stack and stack[-1] > 0 and ast < 0:
-                if stack[-1] > -1*ast:
-                    break
-                elif stack[-1] < -1*ast:
+            # While there is a possibility of collision:
+            # current asteroid is moving left (negative) AND top of stack is moving right (positive)
+            while stack and ast < 0 and stack[-1] > 0:
+                if abs(ast) > abs(stack[-1]):
+                    # Incoming asteroid is bigger, destroy the one on the stack and continue checking
                     stack.pop()
                     continue
-                else:
+                elif abs(ast) == abs(stack[-1]):
+                    # Both are equal, both get destroyed
                     stack.pop()
-                    break
-            # 1,2,3.2
+                # In both above cases, current asteroid does not get added
+                break
             else:
+                # No collision, so just add asteroid to the stack
                 stack.append(ast)
-
-
-        return stack
-
-
-
-
-
-
-# time:O(n), space:O(n)
-class Solution:
-    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        stack = []
-        for asteroid in asteroids:
-            while stack and asteroid <0 and stack[-1] > 0:
-                if stack[-1] == -asteroid:
-                    stack.pop()
-                    break
-                elif stack[-1] < -asteroid:
-                    stack.pop()
-                    continue
-                elif stack[-1] > -asteroid:
-                    break
-            else:
-                stack.append(asteroid)
 
         return stack
