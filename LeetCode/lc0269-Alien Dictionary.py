7f37b9b4b573a30c569c96ef1074b58447ec1843
Rahul Bakshee <rhlbks@gmail.com>
2025-07-16 21:34:16 -0700

    Update lc0269-Alien Dictionary.py

--- LeetCode/lc0269-Alien Dictionary.py
+++ LeetCode/lc0269-Alien Dictionary.py
@@ -1,68 +1,112 @@
 # time:O(C)-C is length of all strings in words added together
-# O(V+E) - Vertex + Edges
-# total time:O(C+V+E)
-
-# space:O(V+E)
-
+# time: O(V+E) - Vertex + Edges,  space:O(V+E)
+# Topological Sort - DFS cycle detection
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
-        graph = {}
-        for word in words:
-            for char in word:
-                graph[char] = []
+        # 1 - build graph for every character in  very word
+        graph = {char:[] for word in words for char in word}
 
-        #print(graph)
-        
-        # build the graph
+        # 2 - iterate over words list pair by pai
+        # try to find if a word is followed by its prefix
         for word1, word2 in zip(words, words[1:]):
-            minLen = min(len(word1), len(word2))
-
-            if len(word1) > len(word2) and word1[:minLen] == word2[:minLen]:
-                    return ""
-            
-            for i in range(minLen):
-                if word1[i] != word2[i]:
-                    graph[word2[i]].append(word1[i])
-                    break
-
-                
-
-        # define dfs
-        def dfs(white, gray, black, node, result):
-            # add to gray
-            gray.add(node)
-
-            for parent in graph[node]:
-                # check if in black
-                if parent in black:
-                    continue
-                
-                # check if in gray
-                if parent in gray:
-                    return False
-                
-                if not dfs(white, gray, black, parent, result):
-                    return False
-            
-
-            # move node from gray to parent as it is fully explored
-            gray.remove(node)
-            black.add(node)
-            result.append(node)
-            return True
-
-        # use three sets
-        white = set(graph.keys())
-        gray = set()
-        black = set()
-        result = []
-
-        # start moving nodes from white to gray to black
-        while white:
-            node = white.pop()
-            if node in black:
-                continue
-            if not dfs(white, gray, black, node, result): # False-if cycle
+            min_len = min(len(word1), len(word2))
+            if len(word1) > len(word2) and word1[:min_len] == word2[:min_len]:
                 return ""
 
+            # iterate over the words and try to find first differentiating char
+            # to update the graph
+            for i in range(min_len):
+                if word1[i] != word2[i]:
+                    graph[word2[i]].append(word1[i]) # same as course schedule
+                    break                            # course:[dependencies]
+                                                     # curr_char:[earlier chars]
+
+        # 3 - run DFS to detect cycle
+        # keep updating the order of alphabets
+        result = []
+
+        white = set(graph.keys())   # To complete/explore
+        black = set()               # already explored/completed nodes
+        gray = set()                # currently processing nodes
+        
+        def dfs(node)->bool:
+            if node in black:
+                return True
+            # check if node is currently bein processed
+            if node in gray:
+                return False
+
+            # add to gray set/currently processing set
+            gray.add(node)
+
+            # explore neighbors
+            for nei in graph[node]:
+                if not dfs(nei):
+                    return False
+
+            # remove from gray and add to black
+            gray.remove(node)
+            black.add(node)
+
+            # update to result
+            result.append(node)
+
+            return True              
+
+        # white -> gray -> black
+        while white:
+            node = white.pop()
+            # check if node already explored
+            if node in black:
+                continue
+
+            if not dfs(node):
+                return ""
+
+        return "".join(result)
+
+
+
+# time:(V+E), space:O(V+E)
+# Kahn algo - toplogical sort using BFS
+class Solution:
+    def alienOrder(self, words: List[str]) -> str:
+        # 1 - init graph
+        graph = {c:set() for word in words for c in word}
+        indegree = {c:0 for c in graph}
+
+        # 2 - populate graph and indegree
+        for word1, word2 in zip(words, words[1:]):
+            min_len = min(len(word1), len(word2))
+            if len(word1) > len(word2) and word1[:min_len] == word2[:min_len]:
+                return ""
+
+            for i in range(min_len):
+                if word1[i] != word2[i]: # differentiator letter
+                    if word2[i] not in graph[word1[i]]:
+                        graph[word1[i]].add(word2[i])
+                        indegree[word2[i]] += 1
+                    break
+
+        
+        # 3 - add all the indegree 0 guys to queue
+        result = []
+        q = deque()
+        for c in indegree:
+            if indegree[c] == 0:
+                q.append(c)
+
+        # start BFS
+        while q:
+            c = q.popleft()
+            result.append(c)
+            
+            for nei in graph[c]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    q.append(nei)
+
+
+        if len(result) < len(graph):
+            return ""
         return "".join(result)
