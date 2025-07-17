b8e72a06b4b883c2215bdae613bf28291cbfbb25
Rahul Bakshee <rhlbks@gmail.com>
2025-07-17 09:28:04 -0700

    Update lc0739-Daily Temperatures.py

--- LeetCode/lc0739-Daily Temperatures.py
+++ LeetCode/lc0739-Daily Temperatures.py
@@ -1,60 +1,34 @@
-# solved this by myself on Feb 17, 2025
-
+# bruteforce - time:O(n^2), space:O(1)
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        stack = []
-        result = [0] * len(temperatures)
+        result = [0 for _ in range(len(temperatures))]
 
-        
-        for i, temp in enumerate(temperatures):
-            if not stack:
-                stack.append([i, temp])
-                continue
-            
-            while stack and temp > stack[-1][1]:
-                prev_i, _ = stack.pop()
-                result[prev_i] = i-prev_i
-                
-                
-            stack.append([i, temp])
-        return result
-                
-                
-                
-######################################
-
-
-
-
-
-
-# https://leetcode.com/problems/daily-temperatures/description/
-
-# brute force  - O(N**2), space:O(1)
-class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        result = [0] * len(temperatures)
-        
         for i in range(len(temperatures)):
             for j in range(i+1, len(temperatures)):
                 if temperatures[j] > temperatures[i]:
                     result[i] = j-i
                     break
-        return result 
+        return result
 
 
-# monotnically decreasing stack
-# time:O(n), space:O(1)
+
+# monotnically decreasing stack - time:O(n), space:O(n)
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        result = [0] * len(temperatures)
-        stack = [] #[temp, index]
+        stack = []
+        result = [0 for _ in range(len(temperatures))]
 
-        for i in range(len(temperatures)):
+        for i, temp in enumerate(temperatures):
+            if not stack:
+                stack.append([temp, i])
+                continue
 
-            while stack and temperatures[i] > stack[-1][0]:
-                stack_temp, stack_index = stack.pop()
-                result[stack_index] = i-stack_index
-            stack.append([temperatures[i], i])
-                
+            while stack and temp > stack[-1][0]:
+                _, index = stack.pop()
+                result[index] = i-index
+                # number of days you have to wait after the ith day
+            
+            stack.append([temp,i])
+
         return result
+                
