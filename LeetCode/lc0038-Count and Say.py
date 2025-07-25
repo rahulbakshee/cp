58a2cfa4a493f93f340d1be50fcdc097c71c67cd
Rahul Bakshee <rhlbks@gmail.com>
2025-07-25 11:59:54 -0700

    Update lc0038-Count and Say.py

--- LeetCode/lc0038-Count and Say.py
+++ LeetCode/lc0038-Count and Say.py
@@ -1,5 +1,32 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
+        if n == 1:
+            return "1"
+
+        curr = "1"
+
+        for _ in range(1, n):
+            next_term = ""
+            i = 0
+
+            while i < len(curr):
+                count = 1
+                while i+1 < len(curr) and curr[i] == curr[i+1]:
+                    count += 1
+                    i += 1
+
+                # append count and digit to the next term
+                next_term += str(count) + curr[i]
+                i += 1
+            curr = next_term
+
+        return curr
+
+#########################################
+
+
+class Solution:
+    def countAndSay(self, n: int) -> str:
         
         def process(num:str)->str:
             result = []
