d6298049d14991279da00016fd0758045225f97a
Rahul Bakshee <rhlbks@gmail.com>
2025-08-26 23:06:07 -0700

    Update lc0295-Find Median from Data Stream.py

--- LeetCode/lc0295-Find Median from Data Stream.py
+++ LeetCode/lc0295-Find Median from Data Stream.py
@@ -25,62 +25,32 @@ class MedianFinder:
             return (self.arr[n//2-1] + self.arr[n//2]) * 0.5
 
 
+class MedianFinder:
+
+    def __init__(self):
+        self.lo = [] # maxHeap storing lower number
+        self.hi = [] # mnHeap storing higher number
+        
+
+    def addNum(self, num: int) -> None:
+        # 1 add directly to lo
+        heapq.heappush(self.lo, -num)
+
+        # 2 - pop from lo and push to hi
+        heapq.heappush(self.hi, -heapq.heappop(self.lo))
+        
+        # 3- check lens
+        if len(self.lo) < len(self.hi):
+            heapq.heappush(self.lo, -heapq.heappop(self.hi))
+
+    def findMedian(self) -> float:
+        if len(self.lo) == len(self.hi):
+            return (-self.lo[0]+self.hi[0])/2
+
+        return -self.lo[0]        
+
 
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
 # obj.addNum(num)
 # param_2 = obj.findMedian()
-
-
-# implement a FindMedian class
-    # def init
-    # def add_num(num)-> None - adds the number to the list
-    # def find_median()-> float
-
-
-
-class MedianFinder:
-    def __init__(self):
-        self.small = [] # smaller eleemnts than middle
-        self.large = [] # larger elements than middle
-
-
-    def addNum(self, num:int)-> None:
-        """adds the integer num from the data stream to the data structure."""
-        # check if input num is largeer than min of largeHeap
-        # if yes then add it to large , if not then add to smallerHeap
-        if self.large and num>self.large[0]:
-            heapq.heappush(self.large, num)
-        else:
-            heapq.heappush(self.small, -1*num)
-
-        # check if len of small is 1 more than large
-        # if not then pop from small and push to large
-        if len(self.small) - len(self.large) >1:
-            # pop from small
-            val = -1 * heapq.heappop(self.small)
-            # push to large
-            heapq.heappush(self.large, val)
-
-
-        # check if len of large is 1 more than small
-        # if not then pop from large and push to small
-        if len(self.large) - len(self.small) >1:
-            # pop from large
-            val = heapq.heappop(self.large)
-            # push to small
-            heapq.heappush(self.small, -1*val)
-    
-# 1, 2, 3, 4
-# 2 small -2,-1
-# 2 large  3, 4
-       
-    def findMedian(self)->float:
-        """ returns the median of all elements so far"""
-        if len(self.small) > len(self.large):
-            return -1*self.small[0]
-        elif len(self.small) < len(self.large):
-            return self.large[0]
-        else:
-            return (-1*self.small[0] + self.large[0])/2
-        
