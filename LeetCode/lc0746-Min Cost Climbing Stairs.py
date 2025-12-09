0addd8c3024ff0ce1e0f0a543978ec1cea10bad4
Rahul Bakshee <rhlbks@gmail.com>
2025-12-09 12:22:45 -0800

    Refactor minCostClimbingStairs function implementations

--- LeetCode/lc0746-Min Cost Climbing Stairs.py
+++ LeetCode/lc0746-Min Cost Climbing Stairs.py
@@ -2,15 +2,14 @@
 # time:O(2^n) breadth^depth, space:O(n)
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        def dp(index):
-            # base case
-            if index >= len(cost):
+        def dfs(i):
+            if i == 0 or i == 1:
                 return 0
-            
-            return min(cost[index] + dp(index+1), cost[index] + dp(index+2))
-        
-        return min(dp(0),dp(1))
 
+            return min(cost[i-1] + dfs(i-1), cost[i-2] + dfs(i-2))
+
+        n = len(cost)
+        return dfs(n)
 
 
 
@@ -18,55 +17,39 @@ class Solution:
 # time:O(n) , space:O(n)
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        def dp(index, memo):
-            # base case
-            if index in memo:
-                return memo[index]
+        def dfs(i):
+            if i in memo:
+                return memo[i]
 
-            if index >= len(cost):
-                return 0
-            
-            memo[index] = min(cost[index] + dp(index+1,memo), cost[index] + dp(index+2,memo))
-            return memo[index]
-        
-        return min(dp(0,{}),dp(1,{}))
+            memo[i] = min(cost[i-1] + dfs(i-1), cost[i-2] + dfs(i-2))
+            return memo[i]
 
-
+        n = len(cost)
+        memo = {0:0, 1:0}
+        return dfs(n)
 
 
 # bottom up DP- tabulation - iterative
 # time:O(n), space:O(n)
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        # edge case - if len of input less than 2
-        if len(cost) < 2:
-            return cost[0]
-
-        # whne len of input is atleast 2 or more
         n = len(cost)
         dp = [0] * (n+1)
 
         for i in range(2, n+1):
-            dp[i] = min(dp[i-1]+cost[i-1], 
-                        dp[i-2]+cost[i-2])
+            dp[i] = min(cost[i-1] + dp[i-1], 
+                        cost[i-2] + dp[i-2])
 
-        return dp[n]
-
+        return dp[-1]
 
 # bottom up DP- space optimized - iterative
 # time:O(n), space:O(1)
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        # edge case - if len of input less than 2
-        if len(cost) < 2:
-            return cost[0]
-
-        # whne len of input is atleast 2 or more
         n = len(cost)
         prev, curr = 0, 0
 
         for i in range(2, n+1):
-            curr, prev = min(curr+cost[i-1], prev+cost[i-2]), curr
+            prev, curr = curr, min(cost[i-2] + prev, cost[i-1] + curr)
 
         return curr
-
