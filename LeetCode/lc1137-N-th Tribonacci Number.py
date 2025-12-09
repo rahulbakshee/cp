a5fe952a2639491c8aceaab57258fc2b3dbe457f
Rahul Bakshee <rhlbks@gmail.com>
2025-12-09 12:32:34 -0800

    Refactor tribonacci function with multiple approaches
    Refactor tribonacci function with different approaches including recursion, memoization, and iterative DP. Improved readability and efficiency.

--- LeetCode/lc1137-N-th Tribonacci Number.py
+++ LeetCode/lc1137-N-th Tribonacci Number.py
@@ -2,38 +2,18 @@
 # time:O(3^n), space:O(n) - 3 is because at every moment 3 options
 class Solution:
     def tribonacci(self, n: int) -> int:
-
-        def dp(n):
-            # base case
-            if n == 0:
+        def dfs(i):
+            if i == 0:
                 return 0
-            if n == 1:
+            if i == 1:
                 return 1
-            if n == 2:
-                return 1
-
-            return dp(n-1) + dp(n-2) + dp(n-3)
-            
-        return dp(n)
-
-
-# recursion + memoization
-# time:O(n), space:O(n)
-class Solution:
-    def tribonacci(self, n: int) -> int:
-        @cache
-        def dp(n):
-            # base case
-            if n == 0:
-                return 0
-            if n == 1:
-                return 1
-            if n == 2:
+            if i == 2:
                 return 1
 
-            return dp(n-1) + dp(n-2) + dp(n-3)
-            
-        return dp(n)
+            return dfs(i-1) + dfs(i-2) + dfs(i-3)
+
+
+        return dfs(n)
 
 
 
@@ -41,65 +21,51 @@ class Solution:
 # time:O(n), space:O(n)
 class Solution:
     def tribonacci(self, n: int) -> int:
+        def dfs(i):
+            if i in memo:
+                return memo[i]
 
-        def dp(n,memo):
-            if n in memo:
-                return memo[n]
-            # base case
-            if n == 0:
-                return 0
-            if n == 1:
-                return 1
-            if n == 2:
-                return 1
+            memo[i] = dfs(i-1) + dfs(i-2) + dfs(i-3)
+            return memo[i]
 
-            memo[n] = dp(n-1,memo) + dp(n-2,memo) + dp(n-3,memo)
-            return memo[n]
-            
-        return dp(n,{})
+        memo = {0:0, 1:1, 2:1}
+        return dfs(n)
+
 
 
 # iterative -DP- tabulation
 # time:O(n), space:O(n)
 class Solution:
     def tribonacci(self, n: int) -> int:
-        # base case
         if n == 0:
             return 0
-        if n == 1:
+        if n == 1 or n == 2:
             return 1
-        if n == 2:
-            return 1
-
+        
         dp = [0] * (n+1)
         dp[0] = 0
         dp[1] = 1
         dp[2] = 1
 
         for i in range(3, n+1):
-            dp[i] = dp[i-1]+dp[i-2]+dp[i-3]
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
 
         return dp[-1]
-
 
 # iterative -DP- tabulation - space optimized
 # time:O(n), space:O(n)
 class Solution:
     def tribonacci(self, n: int) -> int:
-        # base case
         if n == 0:
             return 0
-        if n == 1:
+        if n == 1 or n == 2:
             return 1
-        if n == 2:
-            return 1
-
         
-        prevprev = 0
-        prev = 1
+        prev = 0
+        mid = 1
         curr = 1
 
         for i in range(3, n+1):
-            curr, prev, prevprev = curr+prev+prevprev, curr, prev
+            prev, mid, curr = mid, curr, prev+mid+curr
 
         return curr
