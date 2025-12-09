8efed8c064012c1ed5001c694d510d34ff751485
Rahul Bakshee <rhlbks@gmail.com>
2025-12-09 14:04:47 -0800

    Refactor House Robber solution with DFS and memoization

--- LeetCode/lc0198-House Robber.py
+++ LeetCode/lc0198-House Robber.py
@@ -16,69 +16,83 @@ index  [0,1,2,3,4]
 # space:O(n)
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        def dp(index):
-            # base case
-            if index >= len(nums):
+        
+        def dfs(i):
+            # base 
+            if i >= len(nums):
                 return 0
 
-            return max(nums[index]+dp(index+2), dp(index+1))
+            # not rob it
+            amount = dfs(i+1)
 
-        return max(dp(0), dp(1))
+            # rob it
+            amount = max(amount, nums[i] + dfs(i+2))
 
-
+            return amount
+        
+        return dfs(0)
 
 
 # recursion + memoization - top down DP
 # time:O(n), spacve:O(n)
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        def dp(index, memo={}):
-            # base case
-            if index in memo:
-                return memo[index]
-
-            if index >= len(nums):
+        
+        def dfs(i):
+            if i in memo:
+                return memo[i]
+                
+            # base 
+            if i >= len(nums):
                 return 0
 
-            memo[index] = max(nums[index]+dp(index+2,memo), dp(index+1,memo))
-            return memo[index]
+            # not rob it
+            amount = dfs(i+1)
 
-        return max(dp(0,{}), dp(1,{}))
+            # rob it
+            amount = max(amount, nums[i] + dfs(i+2))
+
+            memo[i] = amount
+            return memo[i]       
+
+
+        memo = {}
+        return dfs(0)
         
 
 # iterative - bottom up  - DP
 # time:O(n), space:O(n)
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) < 2:
-            return nums[-1]
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums)
 
-        # if nums has at least two numbers
         dp = [0] * len(nums)
         dp[0] = nums[0]
         dp[1] = max(nums[0], nums[1])
 
         for i in range(2, len(nums)):
-            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+            dp[i] = max(nums[i]+dp[i-2], dp[i-1])
 
         return dp[-1]
-
-
 
 
 # iterative - bottom up  - DP - space optimized
 # time:O(n), space:O(1)
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) < 2:
-            return nums[-1]
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums)
 
-        # if nums has at least two numbers
+        
         prev = nums[0]
         curr = max(nums[0], nums[1])
 
         for i in range(2, len(nums)):
-            curr, prev = max(prev + nums[i], curr), curr
+            prev, curr = curr, max(nums[i]+prev, curr)
 
         return curr
-
