67ee4158231742b73932226492753fa27c750b25
Rahul Bakshee <rhlbks@gmail.com>
2026-03-02 18:40:12 -0800

    Refactor median finding logic in sorted arrays

--- LeetCode/lc0004-Median of Two Sorted Arrays.py
+++ LeetCode/lc0004-Median of Two Sorted Arrays.py
@@ -19,32 +19,35 @@ class Solution:
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         A, B = nums1, nums2
-        total = len(nums1) + len(nums2)
-        half = total //2
-
-        if len(A) > len(B): # A is smaller
+        if len(A) > len(B):
             A, B = B, A
 
-        left, right = 0, len(A) -1
+        total = len(A) + len(B)
+        half = total // 2
+
+        # binary search on A (smaller)
+        left, right = 0, len(A)-1
         while True:
-            midA = (left + right)//2 
+            midA = (left + right) // 2 
             midB = half - midA - 2
+            
+            leftA = A[midA] if midA >= 0 else float("-inf")
+            rightA = A[midA + 1] if (midA+1) < len(A) else float("inf")
 
-            a_left = A[midA] if midA >=0 else float("-inf")
-            a_right = A[midA+1] if midA+1 < len(A) else float("inf")
-            b_left = B[midB] if midB >= 0 else float("-inf")
-            b_right = B[midB+1] if midB+1 < len(B) else float("inf")
+            leftB = B[midB] if midB >= 0 else float("-inf")
+            rightB = B[midB + 1] if (midB+1) < len(B) else float("inf")
 
-            if a_left <= b_right and b_left <= a_right:
-                if total % 2:
-                    return min(a_right, b_right)
-                return (max(a_left, b_left) + min(a_right, b_right))/2
-            elif a_left > b_left:
-                right = midA -1
+            # compare both the ends from two sublists
+            if leftA <= rightB and leftB <= rightA:
+                # odd  
+                if total %2:
+                    return min(rightA, rightB)
+
+                # even
+                else:
+                    return (max(leftA, leftB) + min(rightA, rightB))/2
+            elif leftA > rightB:
+                right = midA - 1
+
             else:
                 left = midA + 1
-
-
-
-
-                
